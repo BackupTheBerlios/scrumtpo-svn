a945aa97877f6d33d3527eae0b5b3d7125d8c0b7
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-03-22 18:25:42 +0000

    Added new text field that is selected on gained focus.

---
+++ Scrummer/trunk/src/scrummer/uicomponents/SelectedTextField.java
@@ -0,0 +1,43 @@
+package scrummer.uicomponents;
+
+import java.awt.event.FocusEvent;
+
+import javax.swing.JTextField;
+import javax.swing.text.Document;
+
+/**
+ * Text field that selects contents on gotten focus
+ */
+public class SelectedTextField extends JTextField {
+
+	public SelectedTextField() {}
+
+	public SelectedTextField(String text) {
+		super(text);
+	}
+
+	public SelectedTextField(int columns) {
+		super(columns);
+	}
+
+	public SelectedTextField(String text, int columns) {
+		super(text, columns);
+	}
+
+	public SelectedTextField(Document doc, String text, int columns) {
+		super(doc, text, columns);
+	}
+	
+	@Override
+	protected void processFocusEvent(FocusEvent e) {
+		super.processFocusEvent(e);
+		if ( e.getID() == FocusEvent.FOCUS_GAINED )
+		{
+			selectAll() ;
+		}
+		else
+		{
+			select(0, 0);
+		}
+	}
+}
