8e6e8a39c7a4c5923df7fde8d6f883f5f1f81c7b
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-03-27 22:29:04 +0000

    Dodal metodo za poslušalce, ki želijo biti obveščeni o spremembi strani.

--- Scrummer/trunk/src/scrummer/listener/NavigationListener.java
+++ Scrummer/trunk/src/scrummer/listener/NavigationListener.java
@@ -1,8 +1,16 @@
 package scrummer.listener;
 
+import scrummer.model.NavigationModel;
+
 /**
  * Listener to navigation change events
  */
 public interface NavigationListener {
 
+	/**
+	 * Current page was changed
+	 * @param newLink new page link
+	 */
+	void pageChanged(NavigationModel.Link newLink);
+	
 }
