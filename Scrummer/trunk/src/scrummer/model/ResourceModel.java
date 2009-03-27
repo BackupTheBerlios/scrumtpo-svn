9772514c382d49a66072e5c553f5bad879215ad2
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-03-27 22:26:42 +0000

    Dodal nov model.

---
+++ Scrummer/trunk/src/scrummer/model/ResourceModel.java
@@ -0,0 +1,83 @@
+package scrummer.model;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import org.omg.CORBA.portable.ValueInputStream;
+
+import scrummer.IO;
+import scrummer.exception.ValueInvalid;
+
+/**
+ * Resource model encapsulates resource access
+ * 
+ * This model handles image, font and other resources.
+ */
+public class ResourceModel {
+
+	/**
+	 * Images accessable through this model
+	 */
+	public enum Image
+	{
+		/// scrummer logo
+		ScrummerLogo;
+		
+		/**
+		 * Convert enum to filename
+		 * @param image get image string
+		 * @return filename(relative to bin/ directory
+		 */
+		public static String getName(ResourceModel.Image image)
+		{
+			if (image == Image.ScrummerLogo)
+			{
+				return "image/scrum.png".replace("/", IO.separator());
+			}
+			else
+			{
+				throw new ValueInvalid(image.toString(), "Unknown image path!");
+			}
+		}
+	}
+	
+	/**
+	 * Default constructor
+	 * 
+	 * @param logger logger
+	 */
+	public ResourceModel(LoggingModel logger)
+	{
+		_logger = logger;		
+	}
+	
+	/**
+	 * Fetch image
+	 * 
+	 * @param image fetched image
+	 * @return image
+	 * @throws IOException when image could not be loaded
+	 */
+	public BufferedImage get(ResourceModel.Image image) throws IOException
+	{
+		if (!_images.containsKey(image))
+		{
+			BufferedImage bufImage = ImageIO.read(IO.path("file:/", ResourceModel.Image.getName(image)));
+			_images.put(image, bufImage);
+		}
+		System.out.println("TEST");
+		return _images.get(image);
+	}
+	
+	/// image map
+	private HashMap<ResourceModel.Image, BufferedImage> _images = new HashMap<ResourceModel.Image, BufferedImage>();
+	/// logger
+	private LoggingModel _logger;
+}
