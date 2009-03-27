be5fb819d8efdbd94be42e99fdc0c0165e61a46d
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-03-27 22:31:12 +0000

    Precej izboljšal navigacijski gradnik.

--- Scrummer/trunk/src/scrummer/ui/NavigationGridPanel.java
+++ Scrummer/trunk/src/scrummer/ui/NavigationGridPanel.java
@@ -1,20 +1,26 @@
 package scrummer.ui;
 
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import scrummer.Scrummer;
+import scrummer.listener.NavigationListener;
+import scrummer.model.NavigationModel;
+import scrummer.model.NavigationModel.Link;
 
 /**
  * Navigation grid is a panel that contains a fixed grid i.e. it doesn't scale
  * if user resizes it and stays at centre at all times.
  */
-public class NavigationGridPanel extends JPanel implements MouseListener {
+public class NavigationGridPanel extends JPanel implements MouseListener, NavigationListener {
 	
 	/**
 	 * Default constructor
@@ -22,43 +28,13 @@ public class NavigationGridPanel extends JPanel implements MouseListener {
 	public NavigationGridPanel() {
 		super();
 		
-		setLayout(new GridBagLayout());
+		// navigation model
+		_navigationModel = Scrummer.getModels().getNavigationModel();
+		_navigationModel.addConnectionListener(this);
+		setLayout(new GridLayout(1,1));
 		
-		int k = 4;
-		int cellWidth = 128;
-		int cellHeight = 160;
-		
-		int width = k * cellWidth;
-		int height = k * cellHeight;
-		
-		Panel = new JPanel();
-		Panel.setLayout(new GridBagLayout());
-		
-		int panelAdd = 10;
-	
-		limitSize(Panel, width - panelAdd, height - panelAdd);
-		
-		for (int i = 0; i < 6; i++)
-		{
-			ImageTextPanel iep = new ImageTextPanel();
-			iep.setName(new Integer(i).toString());
-			iep.addLabelMouseListener(this);
-			iep.setBorder(BorderFactory.createEmptyBorder(25,25,25,25));
-			
-			GridBagConstraints gc = Util.constraint(GridBagConstraints.BOTH, 1.0, 1.0);
-			gc.fill = GridBagConstraints.BOTH;
-			gc.gridx = i % 2; gc.gridy = i % 3;
-			
-			Panel.add(iep, gc);
-		}
-		
-		JScrollPane scrollPane = new JScrollPane(Panel);
-		// compensate for scrollbar width
-		int add = 10;
-		limitSize(scrollPane, width + add, height + add);
-		GridBagConstraints gc = Util.constraint(GridBagConstraints.CENTER, 1.0, 1.0);		
-		
-		add(scrollPane, gc);
+		_panel = new Box(BoxLayout.Y_AXIS);
+		add(_panel);
 	}
 	
 	/**
@@ -89,8 +65,169 @@ public class NavigationGridPanel extends JPanel implements MouseListener {
 	@Override
 	public void mouseReleased(MouseEvent e) {}
 
+	@Override
+	public void pageChanged(NavigationModel.Link newLink) {
+		// remove all components from panel
+		_panel.removeAll();
+		// add header
+		// _panel.setLayout();
+		
+		Box header = createHeader("Test");
+		// GridBagConstraints headerC = Util.constraint(GridBagConstraints.BOTH, 1.0, 1.0);
+		header.setPreferredSize(new Dimension(1600, 150));
+		header.setMaximumSize(new Dimension(1600, 150));
+		_panel.add(header); // , headerC);
+		
+		JPanel bottomPanel = new JPanel();
+		bottomPanel.setBackground(Color.PINK);		
+		
+		// GridBagConstraints bottomC = Util.constraint(GridBagConstraints.BOTH, 1.0, 3.0);
+		// bottomC.gridy = 1;
+		_panel.add(bottomPanel); // ), bottomC);
+		
+		if (newLink == Link.Blank)
+		{
+			showBlank(bottomPanel);
+		}
+		else if (newLink == Link.Overview)
+		{
+			showOverview(bottomPanel);
+		}
+		else if (newLink == Link.ProductBacklog)
+		{
+			showProductBacklog(bottomPanel);
+		}
+		else if (newLink == Link.Project)
+		{
+			showProject(bottomPanel);
+		}
+		else if (newLink == Link.ProjectDevelopers)
+		{
+			showProjectDevelopers(bottomPanel);
+		}
+		else if (newLink == Link.ProjectOptions)
+		{
+			showProjectOptions(bottomPanel);
+		}
+		else if (newLink == Link.SprintBacklog)
+		{
+			showSprintBacklog(bottomPanel);
+		}
+		else if (newLink == Link.SprintBacklogAbsent)
+		{
+			showSprintBacklogAbsent(bottomPanel);
+		}
+		else if (newLink == Link.SprintBacklogHurdles)
+		{
+			showSprintBacklogHurdles(bottomPanel);
+		}
+		else if (newLink == Link.SprintBacklogMetric)
+		{
+			showSprintBacklogMetric(bottomPanel);
+		}
+		else if (newLink == Link.SprintBacklogTasks)
+		{
+			showSprintBacklogTasks(bottomPanel);
+		}
+		
+		validate();
+		_panel.repaint();
+		repaint();
+	}
+	
+	private void showSprintBacklogTasks(JPanel panel) {}
+
+	private void showSprintBacklogMetric(JPanel panel) {}
+
+	private void showSprintBacklogHurdles(JPanel panel) {}
+
+	private void showSprintBacklogAbsent(JPanel panel) {}
+
+	private void showSprintBacklog(JPanel panel) {}
+
+	private void showProjectOptions(JPanel panel) {}
+
+	private void showProjectDevelopers(JPanel panel) {}
+
+	private void showProject(JPanel panel) {}
+
+	private void showProductBacklog(JPanel panel) {}
+
+	/**
+	 * Show project, sprint backlog, product backlog links
+	 * @param panel panel to manipulate
+	 */
+	private void showOverview(JPanel panel) {}
+
+	/**
+	 * Create header panel with two links(back, home)
+	 * and current section title
+	 * @param title section title
+	 * @return created header
+	 */
+	private Box createHeader(String title)	
+	{
+		Box header = new Box(BoxLayout.Y_AXIS);
+		
+		JPanel linkPanel = new JPanel();
+		linkPanel.setBackground(Color.BLUE);
+		linkPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 0));
+		linkPanel.setMinimumSize(new Dimension(500, 50));
+		linkPanel.setPreferredSize(new Dimension(200, 50));
+		
+		// add back, up, home connection
+		// GrowingLabel backLabel = new GrowingLabel(new Image());
+		
+		
+		/*
+		GridBagConstraints linkPanelC = Util.constraint(GridBagConstraints.BOTH, 1.0, 1.0);
+		linkPanelC.gridy = 0;
+		*/
+		
+		JPanel titlePanel = new JPanel();
+		titlePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		titlePanel.setBackground(Color.YELLOW);
+		titlePanel.setMinimumSize(new Dimension(500, 50));
+		
+		/*
+		GridBagConstraints titlePanelC = Util.constraint(GridBagConstraints.BOTH, 1.0, 1.0);
+		titlePanelC.gridy = 1;
+		*/
+		
+		JLabel titleLabel = new TitleLabel(title);
+		titleLabel.setSize(200, 30);
+		titleLabel.setPreferredSize(new Dimension(200, 30));
+		
+		/*
+		GridBagConstraints titleLabelC = Util.constraint(GridBagConstraints.HORIZONTAL, 1.0, 1.0);
+		titleLabelC.anchor = GridBagConstraints.WEST;
+		titleLabelC.gridy = 0;
+		*/
+		
+		titlePanel.add(titleLabel); // , titleLabelC);
+		
+		header.add(linkPanel);  // ,  linkPanelC);
+		header.add(titlePanel); // , titlePanelC);
+		
+		return header;
+	}
+	
+	/**
+	 * Do nothing stub
+	 * @param panel panel to manipulate
+	 */
+	private void showBlank(JPanel panel) {}
+
+	@Override
+	public void setVisible(boolean flag) {
+		_navigationModel.removeConnectionListener(this);
+		super.setVisible(flag);
+	}
+
 	/// grid panel
-	public JPanel Panel;
+    private Box _panel;
+	/// navigation model
+	private NavigationModel _navigationModel; 
 	/// serialization id
 	private static final long serialVersionUID = -2270654969033851818L;
 }
