61e340668ea60fed4fbe018ff87c269fe0cebab1
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-03-27 22:26:23 +0000

    Dodal kodo, ki se označi ob fokusu.

---
+++ Scrummer/trunk/src/scrummer/uicomponents/SelectedPasswordField.java
@@ -0,0 +1,49 @@
+package scrummer.uicomponents;
+
+import java.awt.event.FocusEvent;
+
+import javax.swing.JPasswordField;
+import javax.swing.text.Document;
+
+/**
+ * Password field that gets selected when it is focused
+ */
+public class SelectedPasswordField extends JPasswordField {
+
+	public SelectedPasswordField() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public SelectedPasswordField(String text) {
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SelectedPasswordField(int columns) {
+		super(columns);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SelectedPasswordField(String text, int columns) {
+		super(text, columns);
+		// TODO Auto-generated constructor stub
+	}
+
+	public SelectedPasswordField(Document doc, String txt, int columns) {
+		super(doc, txt, columns);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void processFocusEvent(FocusEvent e) {
+		super.processFocusEvent(e);
+		if ( e.getID() == FocusEvent.FOCUS_GAINED )
+		{
+			selectAll() ;
+		}
+		else
+		{
+			select(0, 0);
+		}
+	}
+}
