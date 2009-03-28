1fbd4a7b3c18aea4f241e02545a373b34fe8f7ab
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-03-28 21:59:32 +0000

    Več izboljšav za prvo stran.

--- Scrummer/trunk/src/scrummer/ui/NavigationGridPanel.java
+++ Scrummer/trunk/src/scrummer/ui/NavigationGridPanel.java
@@ -1,6 +1,8 @@
 package scrummer.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.ComponentOrientation;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -41,19 +43,6 @@ public class NavigationGridPanel extends JPanel implements MouseListener, Naviga
 		add(_panel);
 	}
 	
-	/**
-	 * Set min, max and preferred size
-	 * @param pane pane for which to set width and height
-	 * @param width width
-	 * @param height height
-	 */
-	private void limitSize(JComponent pane, int width, int height)
-	{
-		pane.setMinimumSize(new Dimension(width, height));
-		pane.setPreferredSize(new Dimension(width, height));
-		pane.setMaximumSize(new Dimension(width, height));			
-	}
-	
 	@Override
 	public void mouseClicked(MouseEvent e) {}
 
@@ -74,61 +63,53 @@ public class NavigationGridPanel extends JPanel implements MouseListener, Naviga
 		// remove all components from panel
 		_panel.removeAll();
 		
-		Box header = createHeader("Test");
+		Box header = createHeader(_navigationModel.getName(newLink));
 		header.setPreferredSize(new Dimension(1600, 110));
 		header.setMaximumSize(new Dimension(1600, 110));
 		_panel.add(header);
 		
 		JPanel bottomPanel = new JPanel();
-		bottomPanel.setBackground(Color.PINK);		
+		bottomPanel.setBackground(Color.WHITE);		
 		
 		_panel.add(bottomPanel);
 		
-		if (newLink == Link.Blank)
+		switch (newLink)
 		{
-			showBlank(bottomPanel);
-		}
-		else if (newLink == Link.Overview)
-		{
-			showOverview(bottomPanel);
-		}
-		else if (newLink == Link.ProductBacklog)
-		{
-			showProductBacklog(bottomPanel);
-		}
-		else if (newLink == Link.Project)
-		{
-			showProject(bottomPanel);
-		}
-		else if (newLink == Link.ProjectDevelopers)
-		{
-			showProjectDevelopers(bottomPanel);
-		}
-		else if (newLink == Link.ProjectOptions)
-		{
-			showProjectOptions(bottomPanel);
-		}
-		else if (newLink == Link.SprintBacklog)
-		{
-			showSprintBacklog(bottomPanel);
-		}
-		else if (newLink == Link.SprintBacklogAbsent)
-		{
-			showSprintBacklogAbsent(bottomPanel);
-		}
-		else if (newLink == Link.SprintBacklogHurdles)
-		{
+		case Blank: 
+			showBlank(bottomPanel); 
+			break;
+		case Overview:
+			showOverview(bottomPanel); 
+			break;
+		case ProductBacklog:
+			showProductBacklog(bottomPanel); 
+			break;
+		case Project:
+			showProject(bottomPanel); 
+			break;
+		case ProjectDevelopers:
+			showProjectDevelopers(bottomPanel); 
+			break;
+		case ProjectOptions:
+			showProjectOptions(bottomPanel); 
+			break;
+		case SprintBacklog:
+			showSprintBacklog(bottomPanel); 
+			break;
+		case SprintBacklogAbsent:
+			showSprintBacklogAbsent(bottomPanel); 
+			break;
+		case SprintBacklogHurdles:
 			showSprintBacklogHurdles(bottomPanel);
-		}
-		else if (newLink == Link.SprintBacklogMetric)
-		{
+			break;
+		case SprintBacklogMetric:
 			showSprintBacklogMetric(bottomPanel);
-		}
-		else if (newLink == Link.SprintBacklogTasks)
-		{
+			break;
+		case SprintBacklogTasks:
 			showSprintBacklogTasks(bottomPanel);
+			break;
 		}
-		
+				
 		validate();
 		_panel.repaint();
 		repaint();
@@ -156,7 +137,28 @@ public class NavigationGridPanel extends JPanel implements MouseListener, Naviga
 	 * Show project, sprint backlog, product backlog links
 	 * @param panel panel to manipulate
 	 */
-	private void showOverview(JPanel panel) {}
+	private void showOverview(JPanel panel) {
+		
+		ResourceModel res = Scrummer.getModels().getResourceModel();
+		
+		panel.setLayout(new BorderLayout());
+		
+		JPanel box = new JPanel();
+		box.setLayout(new FlowLayout(FlowLayout.CENTER));
+		box.setAlignmentX(CENTER_ALIGNMENT);
+		box.setBackground(Color.WHITE);
+		
+		addLink(box, NavigationModel.Link.Project);
+		addLink(box, NavigationModel.Link.ProductBacklog);
+		addLink(box, NavigationModel.Link.SprintBacklog);
+		
+		box.setMaximumSize(new Dimension(1600, 100));
+		
+		Box vertBox = new Box(BoxLayout.X_AXIS);
+		vertBox.add(box);
+		
+		panel.add(vertBox);
+	}
 
 	/**
 	 * Create header panel with two links(back, home)
@@ -174,9 +176,9 @@ public class NavigationGridPanel extends JPanel implements MouseListener, Naviga
 		linkPanel.setMinimumSize(new Dimension(500, 50));
 		linkPanel.setPreferredSize(new Dimension(200, 50));
 		
-		addLabel(linkPanel, ResourceModel.Image.ArrowLeft, "Left", 2);
-		addLabel(linkPanel, ResourceModel.Image.ArrowUp, "Up", 2);
-		addLabel(linkPanel, ResourceModel.Image.Home, "Home", 0);
+		addLabel(linkPanel, ResourceModel.Image.ArrowLeft, TitleLink.Endpoint.Back, 2);
+		addLabel(linkPanel, ResourceModel.Image.ArrowUp,   TitleLink.Endpoint.Up, 2);
+		addLabel(linkPanel, ResourceModel.Image.Home,      TitleLink.Endpoint.Home, 0);
 				
 		JPanel titlePanel = new JPanel();
 		titlePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
@@ -201,22 +203,47 @@ public class NavigationGridPanel extends JPanel implements MouseListener, Naviga
 	 * Add label to panel
 	 * @param panel panel to which to add
 	 * @param image image that will be displayed on label
-	 * @param text text to be written on label
+	 * @param endpoint type of link
 	 * @param addBottomText additional value to add to bottom text offset
 	 */
-	private void addLabel(JPanel panel, ResourceModel.Image image, String text, int addBottomText)
+	private void addLabel(JPanel panel, ResourceModel.Image image, TitleLink.Endpoint endpoint, int addBottomText)
 	{
 		ResourceModel res = Scrummer.getModels().getResourceModel();	
 		// add back, up, home connection
 		try {
-			GrowingLabel label = new GrowingLabel(res.get(image));
-			label.setBorderGrowth(0, 4, 8);
+			GrowingLabel label = new TitleLink(endpoint, res.get(image));
+			label.setBorderGrowth(0, 8, 8);
 			label.setPictureSideOffset(5);
 			label.setPictureTopOffset(5);
 			label.setTextBottomOffset(4 + addBottomText);
 			label.setPreferredSize(new Dimension(64,76));
-			label.setText(text);
+			// label.setText(text);
 			panel.add(label);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Add link to some container
+	 * @param component container
+	 * @param link link to which it points
+	 */
+	private void addLink(JComponent component, NavigationModel.Link link)
+	{
+		ResourceModel res = Scrummer.getModels().getResourceModel();
+		try {
+			scrummer.ui.Link linkControl = 
+				new scrummer.ui.Link(
+					link,
+					res.get(ResourceModel.Image.Sun));
+			linkControl.setBorderGrowth(0, 8, 8);
+			linkControl.setPictureSideOffset(36);
+			linkControl.setPictureTopOffset(5);
+			linkControl.setPreferredSize(new Dimension(64 + 72,76));
+			linkControl.setMaximumSize(new Dimension(64 + 72,76));
+			
+			component.add(linkControl);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
