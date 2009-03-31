144b85ab948a6dcf1d027a1ec738499d8e3dc4a3
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-03-31 22:05:37 +0000

    Usposobil brisanje in urejanje razvijalcev.

--- Scrummer/trunk/src/scrummer/ui/dialog/DevelopersViewDialog.java
+++ Scrummer/trunk/src/scrummer/ui/dialog/DevelopersViewDialog.java
@@ -6,6 +6,7 @@ import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.BorderLayout;
@@ -15,9 +16,11 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JTable;
-import javax.swing.table.TableColumn;
 import org.xnap.commons.i18n.I18n;
 import scrummer.Scrummer;
+import scrummer.enumerator.DataOperation;
+import scrummer.enumerator.DeveloperOperation;
+import scrummer.listener.OperationListener;
 import scrummer.model.DeveloperModel;
 import scrummer.model.swing.DeveloperTableModel;
 import scrummer.ui.Util;
@@ -26,55 +29,35 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.*;
 
-public class DevelopersViewDialog extends JDialog implements MouseListener
+/**
+ * Developer overview and removal dialog
+ */
+public class DevelopersViewDialog 
+	extends JDialog 
+	implements MouseListener, ActionListener, OperationListener<DeveloperOperation>
 {
-	public DevelopersViewDialog(Frame owner) throws SQLException 
+	/**
+	 * Constructor
+	 * @param owner owner frame
+	 */
+	public DevelopersViewDialog(Frame owner) 
 	{	
 		super(owner, ModalityType.APPLICATION_MODAL);
 		
 		setTitle(i18n.tr("View developers"));
-		setSize(new Dimension(820,340));
+		setSize(new Dimension(420,280));
 		
 		JPanel parentPanel = new JPanel();
 		parentPanel.setLayout(new BorderLayout());
-		parentPanel.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 		
 		setLayout(new BorderLayout());
 		
-		/*
-		ConnectionModel cm = Scrummer.getModels().getConnectionModel();
-		final java.sql.Connection con = cm.getConnection();
-		java.sql.Statement stmt = con.createStatement();
-		ResultSet rs = stmt.executeQuery("SELECT * FROM Employee");
-		
-		int id;
-		String name;
-		String surname;
-		String address;
-		DefaultTableModel model = new DefaultTableModel(null,new Object[]{"Team member ID","Name","Surname","Address"})
-		{
-			public boolean isCellEditable(int rowIndex, int mColIndex) 
-			{
-				return false;
-			}
-		};
-		
-		while(rs.next())
-		{
-			id=rs.getInt("Employee_id");
-			name=rs.getString("Employee_name");
-			surname = rs.getString("Employee_surname");
-			address = rs.getString("Employee_address");
-			model.addRow(new Object[]{id,name,surname,address}); 
-		}
-		
-		rs.close();    // All done with that resultset
-	    stmt.close();  // All done with that statement
-	    con.close();  // All done with that DB connection
-	    */
 		DeveloperModel devModel = Scrummer.getModels().getDeveloperModel();
-		DeveloperTableModel model = devModel.getDeveloperTableModel(); 
+		DeveloperTableModel model = devModel.getDeveloperTableModel();
+		_developerTableModel = model;
+		_developerTableModel.addDeveloperListener(this);
 		JTable table = new JTable(model);
+		_developerTable = table;
 		// refresh data from database
 		model.refresh();
 		
@@ -94,39 +77,33 @@ public class DevelopersViewDialog extends JDialog implements MouseListener
 	    		return label;
 	    	}
 	    };
+	    
+	    JScrollPane scrollPane = new JScrollPane(table);
+		parentPanel.add(scrollPane, BorderLayout.CENTER);
 		
-	    /*
-		TableColumn column = null; 
-		for (int i = 0; i < 4; i++) 
-		{
-		    column = table.getColumnModel().getColumn(i);
-		    column.setResizable(false);
-		    
-		    if (i == 1) 
-		        column.setPreferredWidth(140); //third column is bigger
-		    else if(i == 0)
-		    	column.setPreferredWidth(60);
-		    else if(i == 2)
-		    	column.setPreferredWidth(60);
-		    else 
-		        column.setPreferredWidth(80);
-		    
-		    column.setCellRenderer(rdr);
-		    
-		}
-		*/
-		
-		parentPanel.add(table.getTableHeader(), BorderLayout.PAGE_START);
-		parentPanel.add(table, BorderLayout.CENTER);
+		int k = 10;
+		scrollPane.setBorder(
+			Util.createSpacedTitleBorder(
+			k, k, k, k,
+			i18n.tr("Developer table"), 
+			0, k, k, k));
 		
 		JPanel bottomPanel = new JPanel();
-		bottomPanel.setLayout(new FlowLayout(FlowLayout.TRAILING, 0, 0));
+		bottomPanel.setLayout(new FlowLayout(FlowLayout.TRAILING, k, k));
 		bottomPanel.setAlignmentX(RIGHT_ALIGNMENT);
 		
-		JButton backButton = new StandardButton(i18n.tr("Back"));
-		JButton editButton = new StandardButton(i18n.tr("Edit"));
-		bottomPanel.add(backButton);
-		bottomPanel.add(editButton);
+		bottomPanel.setBorder(BorderFactory.createEmptyBorder(k, k, 2, 2));
+		
+		JButton removeButton = new StandardButton(i18n.tr("Remove"));
+		removeButton.setActionCommand("RemoveButton");
+		removeButton.addActionListener(this);
+		
+		JButton closeButton = new StandardButton(i18n.tr("Close"));
+		closeButton.setActionCommand("CloseButton");
+		closeButton.addActionListener(this);
+		
+		bottomPanel.add(removeButton);
+		bottomPanel.add(closeButton);
 		
 		GridBagConstraints bottomGc = Util.constraint(GridBagConstraints.HORIZONTAL, 1.0, 0.3);
 		bottomGc.gridy = 1;
@@ -135,9 +112,42 @@ public class DevelopersViewDialog extends JDialog implements MouseListener
 		
 		Util.centre(this);
 	}
-	
 
-	public void actionPerformed(ActionEvent e) {}
+	public void actionPerformed(ActionEvent e) {
+		String cmd = e.getActionCommand();
+		if (cmd == "RemoveButton")
+		{
+			int selectedRow = _developerTable.getSelectedRow();
+			if (selectedRow == (-1))
+			{
+				JOptionPane.showMessageDialog(
+					this, 
+					i18n.tr("Cannot remove developer because no developer is selected. " +
+							"Click on a table row and then press remove."),
+					i18n.tr("Error"),
+					JOptionPane.ERROR_MESSAGE);
+			}
+			else
+			{
+				_developerTableModel.removeRow(selectedRow);
+			}
+		}
+		else if (cmd == "CloseButton")
+		{
+			setVisible(false);
+		}
+	}
+
+	@Override
+	public void setVisible(boolean b) {
+		
+		if (!b)
+		{
+			_developerTableModel.removeDeveloperListner(this);
+		}
+		
+		super.setVisible(b);
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {}
@@ -152,7 +162,6 @@ public class DevelopersViewDialog extends JDialog implements MouseListener
 		setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 	}
 
-
 	@Override
 	public void mousePressed(MouseEvent e) {}
 
@@ -160,13 +169,27 @@ public class DevelopersViewDialog extends JDialog implements MouseListener
 	@Override
 	public void mouseReleased(MouseEvent e) {}
 	
+	@Override
+	public void operationFailed(DataOperation type, DeveloperOperation identifier, String message) {
+		switch (type)
+		{
+		case Remove:
+			JOptionPane.showMessageDialog(this, message, i18n.tr("Error"), JOptionPane.ERROR_MESSAGE);
+			break;
+		}
+	}
+
+	@Override
+	public void operationSucceeded(DataOperation type, DeveloperOperation identifier, String message) {} 
+	
+	/// developer table
+	private JTable _developerTable;
+	/// developer table model
+	private DeveloperTableModel _developerTableModel;
 	/// serialization id 
 	private static final long serialVersionUID = 456365932759827558L;
-	/// project list
-	// private JList _projectList;
 	/// translation class field
-	private I18n i18n = Scrummer.getI18n(getClass()); 
-	
+	private I18n i18n = Scrummer.getI18n(getClass());	
 }
 	
 
