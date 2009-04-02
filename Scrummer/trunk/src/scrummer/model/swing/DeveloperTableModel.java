6530f57c2fe89798e6fa05f0d7a0ae51d9f5c6f1
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-02 21:35:48 +0000

    Precej popravkov, pometal tabele v DBSchemaModel, pometal skupno funkcionalnost v skupni razred vsem razvijalcem.

--- Scrummer/trunk/src/scrummer/model/swing/DeveloperTableModel.java
+++ Scrummer/trunk/src/scrummer/model/swing/DeveloperTableModel.java
@@ -14,6 +14,7 @@ import scrummer.enumerator.DeveloperOperation;
 import scrummer.listener.OperationListener;
 import scrummer.model.ConnectionModel;
 import scrummer.model.DBSchemaModel;
+import scrummer.model.DeveloperModelCommon;
 import scrummer.model.Models;
 import scrummer.util.ObjectRow;
 import scrummer.util.Operation;
@@ -29,9 +30,11 @@ public class DeveloperTableModel extends DefaultTableModel {
 	 * 
 	 * @param connectionModel connection model to connect to database
 	 */
-	public DeveloperTableModel(ConnectionModel connectionModel) {
+	public DeveloperTableModel(ConnectionModel connectionModel, 
+							   DeveloperModelCommon developerModelCommon,
+							   Operation<DeveloperOperation> operation) {
 		super();
-		_connectionModel = connectionModel;
+		_developerModelCommon = developerModelCommon;
 		
 		_columns.add(i18n.tr("ID"));
 		_columns.add(i18n.tr("Name"));
@@ -58,7 +61,7 @@ public class DeveloperTableModel extends DefaultTableModel {
 	{
 		Models m = Scrummer.getModels();
 		DBSchemaModel schemam = m.getDBSchemaModel();
-		_columns = schemam.getColumns(Employee);
+		_realColumns = schemam.getColumns(DBSchemaModel.EmployeeTable);
 	}
 	
 	/**
@@ -66,19 +69,8 @@ public class DeveloperTableModel extends DefaultTableModel {
 	 */
 	private void refreshTableData()
 	{
-		Query q = new Query(_connectionModel)
-		{
-			@Override
-			public void processResult(ResultSet result) {
-			    _rows = ObjectRow.fetchRows(result);
-	            _rowCount = _rows.size();		
-			}
-			@Override
-			public void handleException(SQLException ex) {
-				ex.printStackTrace();
-			}
-		};
-		q.queryResult("SELECT * FROM " + Employee);
+		_rows = _developerModelCommon.fetchDeveloperTable();
+        _rowCount = _rows.size();
 	}
 
 	@Override
@@ -88,44 +80,21 @@ public class DeveloperTableModel extends DefaultTableModel {
 
 	@Override
 	public void setValueAt(Object value, int row, int column) {
-		
-		Query q = new Query(_connectionModel)
+		if (_developerModelCommon.setDeveloper(
+				_rows.get(row).get(0).toString(),
+				_realColumns.get(column + 1),
+				value.toString()))
 		{
-			@Override
-			public void process() {
-	            refresh();
-			}
-			@Override
-			public void handleException(SQLException ex) {
-				ex.printStackTrace();
-	        	_operation.operationFailed(DataOperation.Update, DeveloperOperation.Developer, 
-	        		i18n.tr("Could not set parameter."));
-			}
-		};
-		String idColumnName = _realColumns.get(0);
-		String columnName = _realColumns.get(column+1);
-		q.query("UPDATE " + Employee + " SET " + columnName + "='" + value.toString() + "' " + 
-            	"WHERE " + idColumnName + "='" + _rows.get(row).get(0) + "'");
+			refresh();
+		}
 	}
 	
 	@Override
 	public void removeRow(int row) {
-	
-		Query q = new Query(_connectionModel)
+		if (_developerModelCommon.removeDeveloper(_rows.get(row).get(0).toString()))
 		{
-			@Override
-			public void process() {
-				refresh();		
-			}
-			@Override
-			public void handleException(SQLException ex) {
-				ex.printStackTrace();
-	        	_operation.operationFailed(DataOperation.Remove, DeveloperOperation.Developer, 
-	        		i18n.tr("Could not remove developer."));
-			}
-		};
-		String idColumnName = _realColumns.get(0);
-		q.query("DELETE FROM " + Employee + " WHERE " + idColumnName + "='" + _rows.get(row).get(0) + "'");
+			refresh();
+		}		
 	}
 
 	@Override
@@ -147,45 +116,22 @@ public class DeveloperTableModel extends DefaultTableModel {
 	public String getColumnName(int column) {
 		return _columns.get(column + 1);
 	}
-
-	/**
-	 * Add developer data change listener
-	 * 
-	 * @param listener listener to add
-	 */
-	public void addDeveloperListener(OperationListener<DeveloperOperation> listener)
-	{
-		_operation.addListener(listener);
-	}
-	
-	/**
-	 * Remove developer data change listener
-	 * @param listener listener to remove
-	 */
-	public void removeDeveloperListner(OperationListener<DeveloperOperation> listener)
-	{
-		_operation.removeListener(listener);
-	}
 	
 	/// column count
 	private int _columnCount = 3;
 	/// row count
 	private int _rowCount = 0;
-	/// connection handler
-	private ConnectionModel _connectionModel;
 	/// column names for display
 	private Vector<String> _columns = new Vector<String>(4);
 	/// real column names for UPDATE-ing
 	private Vector<String> _realColumns = new Vector<String>();
 	/// data rows
 	private Vector<ObjectRow> _rows = new Vector<ObjectRow>();
-	/// data listener
-	private Operation<DeveloperOperation> _operation = new Operation<DeveloperOperation>();
+	/// common developer model operations
+	private DeveloperModelCommon _developerModelCommon;
 	/// translation class field
 	private I18n i18n = Scrummer.getI18n(getClass());
 	/// serialization id
 	private static final long serialVersionUID = 2334976808166694864L;
-	/// table name
-	public static final String Employee = "Employee";
 	
 }
