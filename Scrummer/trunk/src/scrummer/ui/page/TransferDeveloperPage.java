1edb4b9f3d71ebbebc372d1c1af272f7df584398
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-03 21:48:33 +0000

    Premaknil ui za prenos razvijalcev v svojo kontrolo.

--- Scrummer/trunk/src/scrummer/ui/page/TransferDeveloperPage.java
+++ Scrummer/trunk/src/scrummer/ui/page/TransferDeveloperPage.java
@@ -18,6 +18,7 @@ import javax.swing.JPanel;
 import org.xnap.commons.i18n.I18n;
 
 import scrummer.Scrummer;
+import scrummer.ui.ListInterchangePanel;
 import scrummer.ui.Util;
 import scrummer.uicomponents.StandardButton;
 
@@ -37,70 +38,10 @@ public class TransferDeveloperPage extends JPanel {
 		setBackground(Color.WHITE);
 		setLayout(new GridBagLayout());
 		
-		int k = 10;
-		
-		JList freeDevList = createList();
-		freeDevList.setBorder(
-			Util.createSpacedTitleBorder(
-				k, k, k, k, 
-				i18n.tr("Employees"), 
-				k, k, k, k));
-		freeDevList.setPreferredSize(new Dimension(200,200));
-		
-		GridBagConstraints freeDevListC = Util.constraint(GridBagConstraints.VERTICAL, 2.0, 1.0);
-
-		JPanel midPanel = new JPanel(new GridLayout(2,1));
-		midPanel.setBackground(Color.WHITE);
-		midPanel.setPreferredSize(new Dimension(200, 200));
-		midPanel.setAlignmentY(CENTER_ALIGNMENT);
-		
-		GridBagConstraints midPanelC = Util.constraint(GridBagConstraints.VERTICAL, 1.0, 1.0);
-		
-		Box moveRightBox = new Box(BoxLayout.Y_AXIS);
-		moveRightBox.setMaximumSize(new Dimension(200,200));
-				
-		JButton moveRightButton = new StandardButton(">");
-		moveRightButton.setSize(new Dimension(60,40));
-		moveRightButton.setActionCommand("MoveRight");
-		moveRightButton.setAlignmentY(TOP_ALIGNMENT);
-		
-		Box innerMoveRightBox = new Box(BoxLayout.X_AXIS);
-		innerMoveRightBox.setMaximumSize(new Dimension(40,200));
-		innerMoveRightBox.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
-		
-		innerMoveRightBox.add(moveRightButton);
-		moveRightBox.add(innerMoveRightBox);
-		
-		Box moveLeftBox = new Box(BoxLayout.Y_AXIS);
-		moveLeftBox.setMaximumSize(new Dimension(200, 200));
-		
-		Box innerMoveLeftBox = new Box(BoxLayout.X_AXIS);
-		innerMoveLeftBox.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
-		innerMoveLeftBox.setMaximumSize(new Dimension(40, 200));
-		
-		JButton moveLeftButton = new StandardButton("<");
-		moveLeftButton.setSize(new Dimension(60, 40));
-		moveLeftButton.setAlignmentY(BOTTOM_ALIGNMENT);
-		moveLeftButton.setActionCommand("MoveLeft");		
-		
-		innerMoveLeftBox.add(moveLeftButton);
-		moveLeftBox.add(innerMoveLeftBox);
-		
-		midPanel.add(moveLeftBox);
-		midPanel.add(moveRightBox);
-		
-		JList projectDevList = createList();
-		projectDevList.setBorder(
-			Util.createSpacedTitleBorder(
-				k, k, k, k, 
-				i18n.tr("Project Employees"), 
-				k, k, k, k));
-		projectDevList.setPreferredSize(new Dimension(200,200));
-		GridBagConstraints projectDevListC = Util.constraint(GridBagConstraints.VERTICAL, 2.0, 1.0);
-		
-		add(freeDevList,    freeDevListC);
-		add(midPanel,       midPanelC);
-		add(projectDevList, projectDevListC);
+		add(new ListInterchangePanel(
+			new GridLayout(1,1), 
+			i18n.tr("Employee"), 
+			i18n.tr("Project Employee")));
 	}
 	
 	/**
