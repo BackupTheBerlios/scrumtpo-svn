768beb12be56a3b44ea07f078f10249863ff5a89
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-04 11:49:03 +0000

    Dodal več funkcij za delo s timi.

--- Scrummer/trunk/src/scrummer/model/DeveloperModel.java
+++ Scrummer/trunk/src/scrummer/model/DeveloperModel.java
@@ -1,11 +1,13 @@
 package scrummer.model;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import scrummer.enumerator.DataOperation;
+import java.util.Vector;
+
 import scrummer.enumerator.DeveloperOperation;
 import scrummer.listener.OperationListener;
+import scrummer.model.DBSchemaModel.IdValue;
 import scrummer.model.swing.DeveloperTableModel;
+import scrummer.model.swing.EmployeeListModel;
+import scrummer.model.swing.TeamComboBoxModel;
 import scrummer.util.Operation;
 
 /**
@@ -30,9 +32,16 @@ public class DeveloperModel {
 		}
 		/// connection model
 		_connectionModel = connectionModel;
-		_developerModelCommon = new DeveloperModelCommon(_connectionModel, _operation);
-		_developerTableModel = new DeveloperTableModel(connectionModel, _developerModelCommon, _operation);
-		
+		_developerModelCommon = 
+			new DeveloperModelCommon(_connectionModel, _operation);
+		_developerTableModel = 
+			new DeveloperTableModel(connectionModel, _developerModelCommon, _operation);
+		_employeeListModelA = 
+			new EmployeeListModel(_developerModelCommon);
+		_employeeListModelB = 
+			new EmployeeListModel(_developerModelCommon);
+		_teamComboBoxModel = 
+			new TeamComboBoxModel(_developerModelCommon);
 	}
 	
 	/**
@@ -47,6 +56,76 @@ public class DeveloperModel {
 	}
 	
 	/**
+	 * Move selected indices from A to B
+	 * @param indices indices of elements in A
+	 */
+	public void moveAtoB(int [] indices)
+	{
+		Vector<IdValue> moved = new Vector<IdValue>();
+		for (int i = 0; i < indices.length; i++)
+		{
+			moved.add(_employeeListModelA.getValue(indices[i]));
+		}
+		
+		for (int i = 0; i < moved.size(); i++)
+		{
+			_employeeListModelA.removeElement(moved.get(i));
+		}
+		for (int i = 0; i < moved.size(); i++)
+		{
+			_employeeListModelB.addValue(moved.get(i));
+		}
+	}
+	
+	/**
+	 * Move selected indices from B to A
+	 * @param indices indices of elements in B
+	 */
+	public void moveBtoA(int [] indices)
+	{
+		Vector<IdValue> moved = new Vector<IdValue>();
+		for (int i = 0; i < indices.length; i++)
+		{
+			moved.add(_employeeListModelB.getValue(indices[i]));
+		}
+		
+		for (int i = 0; i < moved.size(); i++)
+		{
+			_employeeListModelB.removeElement(moved.get(i));
+		}
+		for (int i = 0; i < moved.size(); i++)
+		{
+			_employeeListModelA.addValue(moved.get(i));
+		}
+	}	
+	
+	/**
+	 * Add team with name from B employee list
+	 * 
+	 * @param name team name
+	 */
+	public void addTeamB(String name)
+	{
+		// assemble all id's
+		Vector<Integer> employees = new Vector<Integer>();
+		for (int i = 0; i < _employeeListModelB.size(); i++)
+		{
+			employees.add(_employeeListModelB.getValue(i).Id);
+		}
+		_developerModelCommon.addTeam(name, employees);
+	}
+	
+	/**
+	 * Remove team by id
+	 * 
+	 * @param id team id
+	 */
+	public void removeTeam(int id)
+	{
+		_developerModelCommon.removeTeam(id);
+	}
+	
+	/**
 	 * Developer table model
 	 * 
 	 * @return developer table model
@@ -54,6 +133,33 @@ public class DeveloperModel {
 	public DeveloperTableModel getDeveloperTableModel()
 	{
 		return _developerTableModel;
+	}
+	
+	/**
+	 * Employee list model - contains id's + employees 
+	 * @return model
+	 */
+	public EmployeeListModel getEmployeeListModelA()
+	{
+		return _employeeListModelA;
+	}
+	
+	/**
+	 * Employee list model - contains id's + employees 
+	 * @return model
+	 */
+	public EmployeeListModel getEmployeeListModelB()
+	{
+		return _employeeListModelB;
+	}
+	
+	/**
+	 * Fetch team combo box model(contains all team names)
+	 * @return model
+	 */
+	public TeamComboBoxModel getTeamComboBoxModel()
+	{
+		return _teamComboBoxModel;
 	}
 	
 	/**
@@ -81,6 +187,12 @@ public class DeveloperModel {
 	private ConnectionModel _connectionModel;
 	/// developer table model
 	private DeveloperTableModel _developerTableModel;
+	/// employee list model
+	private EmployeeListModel _employeeListModelA;
+	/// employee list model - contains all employees except those in A
+	private EmployeeListModel _employeeListModelB;
+	/// team combo box model
+	private TeamComboBoxModel _teamComboBoxModel;
 	/// developer operation
 	private Operation<DeveloperOperation> _operation = new Operation<DeveloperOperation>();
 }
