50bb8dd61a4c91428bfcc1eafea36021b36a30b4
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-04 11:50:13 +0000

    Precej izboljšal obrazec za dodajanje skupin.

--- Scrummer/trunk/src/scrummer/ui/dialog/TeamAddDialog.java
+++ Scrummer/trunk/src/scrummer/ui/dialog/TeamAddDialog.java
@@ -6,16 +6,26 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import org.xnap.commons.i18n.I18n;
 
 import scrummer.Scrummer;
+import scrummer.enumerator.DataOperation;
+import scrummer.enumerator.DeveloperOperation;
+import scrummer.listener.OperationListener;
+import scrummer.model.DeveloperModel;
+import scrummer.model.DeveloperModelCommon;
+import scrummer.model.Models;
+import scrummer.model.swing.EmployeeListModel;
 import scrummer.ui.ListInterchangePanel;
 import scrummer.ui.Util;
 import scrummer.uicomponents.SelectedFormattedTextField;
@@ -24,7 +34,8 @@ import scrummer.uicomponents.TwoButtonDialog;
 /**
  * Add team dialog
  */
-public class TeamAddDialog extends TwoButtonDialog{
+public class TeamAddDialog extends TwoButtonDialog
+						   implements OperationListener<DeveloperOperation> {
 
 	/**
 	 * Constructor
@@ -35,6 +46,12 @@ public class TeamAddDialog extends TwoButtonDialog{
 	{
 		super(parent, ModalityType.APPLICATION_MODAL);
 		setTitle(i18n.tr("Add Team"));
+		
+		Models m = Scrummer.getModels();
+		_developerModel = m.getDeveloperModel();
+		_developerModel.addDeveloperListener(this);
+		_employeeListModelLeft = _developerModel.getEmployeeListModelA();
+		_employeeListModelRight = _developerModel.getEmployeeListModelB();
 		
 		Panel.setLayout(new GridBagLayout());
 		
@@ -58,17 +75,18 @@ public class TeamAddDialog extends TwoButtonDialog{
 				new GridBagLayout(),
 				i18n.tr("Employee"),
 				i18n.tr("Team Member"));
+		ListPanel = interchangePanel;
 		GridBagConstraints interchangePanelC = Util.constraint(GridBagConstraints.BOTH, 1.0, 4.0);
 		interchangePanelC.gridy = 1;
 		
 		Color backColor = Color.getColor("Panel.background");
 		
-		interchangePanel.LeftList.setPreferredSize(new Dimension(200, 140));
-		interchangePanel.LeftList.setBackground(backColor);
-		interchangePanel.MiddlePanel.setPreferredSize(new Dimension(20, 140));
-		interchangePanel.MiddlePanel.setBackground(backColor);
-		interchangePanel.RightList.setPreferredSize(new Dimension(200,140));
-		interchangePanel.RightList.setBackground(backColor);
+		ListPanel.LeftList.setPreferredSize(new Dimension(200, 140));
+		ListPanel.LeftList.setBackground(backColor);
+		ListPanel.MiddlePanel.setPreferredSize(new Dimension(20, 140));
+		ListPanel.MiddlePanel.setBackground(backColor);
+		ListPanel.RightList.setPreferredSize(new Dimension(200,140));
+		ListPanel.RightList.setBackground(backColor);
 		
 		Panel.add(topPanel, topPanelC);
 		Panel.add(interchangePanel, interchangePanelC);
@@ -76,13 +94,118 @@ public class TeamAddDialog extends TwoButtonDialog{
 		OK.setText(i18n.tr("Add"));
 		BottomPanel.setBorder(BorderFactory.createEmptyBorder(k - 5,k,k - 5,k - 5));
 		
+		ListPanel.LeftList.setModel(_employeeListModelLeft);
+		_employeeListModelLeft.refresh();
+		ListPanel.LeftList.invalidate();
+		
+		_employeeListModelRight.removeAllElements();
+		ListPanel.RightList.setModel(_employeeListModelRight);
+		
+		ListPanel.MoveLeftButton.setActionCommand("MoveLeft");
+		ListPanel.MoveLeftButton.addActionListener(this);
+		
+		ListPanel.MoveRightButton.setActionCommand("MoveRight");
+		ListPanel.MoveRightButton.addActionListener(this);
+		
 		setSize(new Dimension(440, 280));
 	}
 	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand() == "MoveLeft")
+		{
+			if (!ListPanel.RightList.isSelectionEmpty())
+			{
+				// check for selection in right model
+				int [] selection = ListPanel.RightList.getSelectedIndices();
+				_developerModel.moveBtoA(selection);
+			}
+		}
+		else if (e.getActionCommand() == "MoveRight")
+		{
+			if (!ListPanel.LeftList.isSelectionEmpty())
+			{
+				// check for selection in right model
+				int [] selection = ListPanel.LeftList.getSelectedIndices();
+				_developerModel.moveAtoB(selection);
+			}
+		}
+		else if (e.getActionCommand() == "StandardDialog.OK")
+		{
+			String text = _teamNameInput.getText().trim();
+			if (text.length() == 0)
+			{
+				JOptionPane.showMessageDialog(
+				    this, 
+				    i18n.tr("Team name must contain at least one character."), 
+				    i18n.tr("Error"), JOptionPane.ERROR_MESSAGE);
+			}
+			else
+			{
+				_developerModel.addTeamB(text);
+			}
+		}
+		else
+		{
+			super.actionPerformed(e);
+		}
+	}
+	
+	@Override
+	public void operationFailed(DataOperation type, DeveloperOperation identifier, String message) {
+		switch (type)
+		{
+		case Insert:
+			switch (identifier)
+			{
+			case Team:
+				JOptionPane.showMessageDialog(
+					this, 
+					i18n.tr("Could not delete team: " + message), 
+					i18n.tr("Error"), 
+					JOptionPane.ERROR_MESSAGE);
+				break;
+			}			
+			break;
+		}
+	}
+
+	@Override
+	public void operationSucceeded(DataOperation type, DeveloperOperation identifier, String message) {
+		switch (type)
+		{
+		case Insert:
+			switch (identifier)
+			{
+			case Team:	
+				setVisible(false);
+				break;
+			}			
+			break;
+		}
+	}	
+
+	@Override
+	public void setVisible(boolean b) {
+
+		if (!b)
+		{
+			_developerModel.removeDeveloperListener(this);
+		}
+		
+		super.setVisible(b);
+	}
+
+	/// developer model
+	private DeveloperModel _developerModel;
+	/// employee list model
+	private EmployeeListModel _employeeListModelLeft, _employeeListModelRight;
+	/// list interchange panel
+	private ListInterchangePanel ListPanel;
 	/// team name
 	private JTextField _teamNameInput;
 	/// translation class field
 	private I18n i18n = Scrummer.getI18n(getClass()); 
 	/// serialization id
-	private static final long serialVersionUID = -1026179270145279297L;	
+	private static final long serialVersionUID = -1026179270145279297L;
 }
