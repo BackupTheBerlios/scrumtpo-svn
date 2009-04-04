d8216d047ec6fe4fadf5ae2d115199766816da75
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-04 11:49:22 +0000

    Dodal več funkcij za delo s timi.

--- Scrummer/trunk/src/scrummer/model/DeveloperModelCommon.java
+++ Scrummer/trunk/src/scrummer/model/DeveloperModelCommon.java
@@ -6,35 +6,18 @@ import java.util.Vector;
 import scrummer.Scrummer;
 import scrummer.enumerator.DataOperation;
 import scrummer.enumerator.DeveloperOperation;
+import scrummer.enumerator.ProjectOperation;
+import scrummer.model.DBSchemaModel.IdValue;
 import scrummer.util.ObjectRow;
 import scrummer.util.Operation;
 import scrummer.util.Query;
 import scrummer.util.ResultQuery;
+import sun.security.pkcs11.Secmod.DbMode;
 
 /**
  * This model features common developer related functionality
  */
 public class DeveloperModelCommon {
-	
-	/**
-	 * Id and name struct
-	 */
-	public static class IdName 
-	{
-		/**
-		 * Constructor
-		 * @param id id
-		 * @param name name
-		 */
-		public IdName(int id, String name)
-		{
-			Id   = id;
-			Name = name;
-		}
-		
-		public int Id;
-		public String Name;
-	}
 	
 	/**
 	 * Constructor
@@ -86,6 +69,49 @@ public class DeveloperModelCommon {
         	 _connectionModel.close(st);
         	 _connectionModel.close(conn);
          }
+	}
+	
+	/**
+	 * Fetch developers and return full names + ids
+	 * 
+	 * @return identified devs
+	 */
+	public Vector<IdValue> fetchDeveloperNames()
+	{
+		ResultQuery<Vector<IdValue>> q = new ResultQuery<Vector<IdValue>>(_connectionModel)
+		{
+			@Override
+			public void processResult(ResultSet result) {
+				Vector<IdValue> res = new Vector<IdValue>();
+				try {
+					result.beforeFirst();
+					while (result.next())
+					{
+						res.add(new IdValue(result.getInt(1), result.getString(2)));
+					}
+					setResult(res);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			@Override
+			public void handleException(SQLException ex) {
+				ex.printStackTrace();
+			}
+		};
+		q.queryResult(
+			"SELECT " + DBSchemaModel.EmployeeId + ", " + 
+			"CONCAT(" + DBSchemaModel.EmployeeName + " , ' ', " +
+						DBSchemaModel.EmployeeSurname + ") " +
+			"FROM "   + DBSchemaModel.EmployeeTable);
+		if (q.getResult() == null)
+		{
+			return new Vector<IdValue>();
+		}
+		else
+		{
+			return q.getResult();
+		}
 	}
 	
 	/**
@@ -190,7 +216,7 @@ public class DeveloperModelCommon {
 	 * @param projectId project id
 	 * @return devs
 	 */
-	public Vector<IdName> fetchProjectDevelopers(int projectId)
+	public Vector<IdValue> fetchProjectDevelopers(int projectId)
 	{
 		/*
 		ResultQuery<Vector<IdName>> q = new ResultQuery<Vector<IdName>>(_connectionModel)
@@ -210,6 +236,189 @@ public class DeveloperModelCommon {
 		return q.getResult();
 		*/
 		return null;
+	}
+	
+	/**
+	 * Add team into database given following data
+	 * 
+	 * @param name team name
+	 * @param employeeIds employee id's
+	 */
+	public void addTeam(String name, Vector<Integer> employeeIds)
+	{
+		// TODO: this naively doesn't use transactions, implement it
+		boolean cont = true;
+		
+		java.sql.Connection conn      = null;
+        java.sql.PreparedStatement st = null;
+        ResultSet res = null;
+        try {
+        	
+            conn = _connectionModel.getConnection();
+            
+            String query =
+            	"INSERT INTO " + DBSchemaModel.TeamTable + " " +
+            	"(" + DBSchemaModel.TeamName + ") " +
+                "VALUES (?)";
+            st = conn.prepareStatement(query);
+            st.setString(1, name);
+            st.execute();
+              
+            _connectionModel.close(st);      
+        } catch (SQLException e) {
+            e.printStackTrace();
+            _operation.operationFailed(DataOperation.Insert, DeveloperOperation.Team, e.getMessage());
+            cont = false;
+        }
+        finally {
+	       	_connectionModel.close(res);
+	       	_connectionModel.close(st);
+	       	_connectionModel.close(conn);
+        }
+		
+        if (cont)
+        {
+			ResultQuery<String> q = new ResultQuery<String>(_connectionModel)
+			{
+				@Override
+				public void processResult(ResultSet result) throws SQLException {
+									
+					result.beforeFirst();
+					while (result.next())
+					{
+						setResult(result.getString(1));
+					}
+				}
+				
+				@Override
+				public void handleException(SQLException ex) {
+					ex.printStackTrace();
+					setResult(null);
+					_operation.operationFailed(DataOperation.Insert, DeveloperOperation.Team, ex.getMessage());
+				}	
+			};
+			q.queryResult(
+				"SELECT " + DBSchemaModel.TeamId    + " "  + 
+				"FROM "   + DBSchemaModel.TeamTable + " "  +
+				"WHERE "  + DBSchemaModel.TeamName  + "='" + name + "'");
+	        
+			if (q.getResult() != null)
+			{
+				conn = null;
+		        st   =  null;
+		        res  = null;
+		        try {
+		        	
+		            conn = _connectionModel.getConnection();
+		            String query = 
+		            	"INSERT INTO " + DBSchemaModel.TeamMemberTable + " " +
+		            	"(" + DBSchemaModel.TeamMemberEmployeeId + ", " + 
+		            		  DBSchemaModel.TeamMemberTeamId + ") " +
+		            	"VALUES (?, ?)";
+		            
+		            st = conn.prepareStatement(query);
+		            for (int i = 0; i < employeeIds.size(); i++)
+		            {
+		            	st.setString(1, q.getResult());	
+		            	st.setInt(2, employeeIds.get(i));
+		            	st.execute();
+		            }
+		        
+		            _connectionModel.close(st);
+		            _operation.operationSucceeded(DataOperation.Insert, DeveloperOperation.Team, "");
+		        } catch (SQLException e) {
+		            e.printStackTrace();
+		            _operation.operationFailed(DataOperation.Insert, DeveloperOperation.Team, e.getMessage());
+		        }
+		        finally {
+			       	_connectionModel.close(res);
+			       	_connectionModel.close(st);
+			       	_connectionModel.close(conn);
+		        }
+			}
+        }
+	}
+	
+	/**
+	 * Fetch all teams
+	 * 
+	 * @return identified teams
+	 */
+	public Vector<IdValue> fetchTeamNames()
+	{
+		ResultQuery<Vector<IdValue>> q = new ResultQuery<Vector<IdValue>>(_connectionModel)
+		{
+			@Override
+			public void processResult(ResultSet result) {
+				setResult(IdValue.fetchValues(result));
+			}
+			@Override
+			public void handleException(SQLException ex) {
+				ex.printStackTrace();
+			}
+		};
+		q.queryResult(
+			"SELECT " + DBSchemaModel.TeamId + ", " + DBSchemaModel.TeamName + " " +
+			"FROM "   + DBSchemaModel.TeamTable);
+		
+		if (q.getResult() == null)
+		{
+			return new Vector<IdValue>();
+		}
+		else
+		{
+			return q.getResult();
+		}
+	}
+	
+	/**
+	 * Remove team
+	 * 
+	 * @param id team id
+	 */
+	public void removeTeam(int id)
+	{
+		ResultQuery<Boolean> q = new ResultQuery<Boolean>(_connectionModel)
+		{
+			@Override
+			public void process() {
+				setResult(true);
+			}
+			@Override
+			public void handleException(SQLException ex) {
+				setResult(false);
+				ex.printStackTrace();
+				_operation.operationFailed(DataOperation.Remove, DeveloperOperation.Team, ex.getMessage());
+			}
+		};
+		q.query(
+			"DELETE FROM " + DBSchemaModel.TeamMemberTable + " " +
+			" WHERE " + DBSchemaModel.TeamMemberTeamId + "='" + id + "'"); 
+		
+		if (q.getResult())
+		{
+			
+			q = new ResultQuery<Boolean>(_connectionModel)
+			{
+				@Override
+				public void process() {
+					setResult(true);
+				}
+				@Override
+				public void handleException(SQLException ex) {
+					setResult(false);
+					ex.printStackTrace();
+					_operation.operationFailed(DataOperation.Remove, DeveloperOperation.Team, ex.getMessage());
+				}
+			};
+			q.query(
+				"DELETE FROM " + DBSchemaModel.TeamTable + " " +
+				" WHERE " + DBSchemaModel.TeamId + "='" + id + "'");			
+		}
+		if (q.getResult())
+		{
+			_operation.operationSucceeded(DataOperation.Remove, DeveloperOperation.Team, "");
+		}
 	}
 	
 	/// connection model
