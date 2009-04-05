5b4f886cb9b7de39ec209f50b56dbba2ccef2016
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-05 11:00:10 +0000

    Dodal razred za seznam vseh razvijalcev, ki niso v podani razvojni skupini.

---
+++ Scrummer/trunk/src/scrummer/model/swing/DeveloperNonTeamListModel.java
@@ -0,0 +1,38 @@
+package scrummer.model.swing;
+
+import scrummer.model.DeveloperModelCommon;
+
+/**
+ * Model for all employees that are not on some team
+ */
+public class DeveloperNonTeamListModel extends DeveloperTeamBaseListModel {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param teamId team id
+	 * @param developerModelCommon common developer model
+	 */
+	public DeveloperNonTeamListModel(int teamId, DeveloperModelCommon developerModelCommon)
+	{
+		super(teamId, developerModelCommon);
+	}
+
+	@Override
+	public void refresh() {
+	
+		refreshRows();
+		super.refresh();
+	}
+	
+	/**
+	 * Refresh developers
+	 */
+	private void refreshRows()
+	{
+		setEmployees(getDeveloperModelCommon().fetchNonTeamMembers(getTeam()));
+	}
+	
+	/// serialization id
+	private static final long serialVersionUID = 4910864066463713374L;
+}
