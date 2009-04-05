51979401a51655786fab34cb6b0e56b7d5de663d
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-05 09:37:10 +0000

    Precej izboljšal obrazec za urejanje skupin.

--- Scrummer/trunk/src/scrummer/ui/dialog/TeamOverviewDialog.java
+++ Scrummer/trunk/src/scrummer/ui/dialog/TeamOverviewDialog.java
@@ -9,6 +9,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Window;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
@@ -18,7 +20,12 @@ import javax.swing.JTextField;
 
 import org.xnap.commons.i18n.I18n;
 
+import com.sun.org.apache.xerces.internal.xs.ItemPSVI;
+
 import scrummer.Scrummer;
+import scrummer.model.DeveloperModel;
+import scrummer.model.swing.DeveloperTeamListModel;
+import scrummer.model.swing.TeamComboBoxModel;
 import scrummer.ui.ListInterchangePanel;
 import scrummer.ui.Util;
 import scrummer.uicomponents.SelectedFormattedTextField;
@@ -27,7 +34,8 @@ import scrummer.uicomponents.TwoButtonDialog;
 /**
  * Team overview dialog makes team editing easier
  */
-public class TeamOverviewDialog extends TwoButtonDialog {
+public class TeamOverviewDialog extends TwoButtonDialog
+								implements ItemListener {
 
 	/**
 	 * Constructor
@@ -37,6 +45,10 @@ public class TeamOverviewDialog extends TwoButtonDialog {
 		super(owner, ModalityType.APPLICATION_MODAL);
 		setTitle(i18n.tr("Add Team"));
 		
+		_developerModel         = Scrummer.getModels().getDeveloperModel();
+		_developerTeamListModel = _developerModel.getDeveloperTeamListModel();
+		_teamComboBoxModel      = _developerModel.getTeamComboBoxModel();
+		
 		Panel.setLayout(new GridBagLayout());
 		
 		int k = 15;
@@ -45,29 +57,22 @@ public class TeamOverviewDialog extends TwoButtonDialog {
 		topPanel.setBorder(BorderFactory.createEmptyBorder(k, k, 0, k));
 		GridBagConstraints topPanelC = Util.constraint(GridBagConstraints.HORIZONTAL, 1.0, 1.0);
 		
-		/*
-		JLabel nameLbl = new JLabel(i18n.tr("Team name") + ":");
-
-		SelectedFormattedTextField nameTextInput = new SelectedFormattedTextField();
-		nameTextInput.setColumns(14);
-		*/
-		
 		JLabel teamLbl = new JLabel(i18n.tr("Team") + ":");
 		JComboBox teamCbbx = new JComboBox();
+		teamCbbx.setModel(_teamComboBoxModel);
+		_teamComboBoxModel.refresh();
+		teamCbbx.addItemListener(this);
+		_teamInput = teamCbbx;
 		
 		topPanel.add(teamLbl);		
 		topPanel.add(teamCbbx);
-		
-		/*
-		topPanel.add(nameLbl);		
-		topPanel.add(nameTextInput);
-		*/
 		
 		ListInterchangePanel interchangePanel = 
 			new ListInterchangePanel(
 				new GridBagLayout(),
 				i18n.tr("Employee"),
 				i18n.tr("Team Member"));
+		ListPanel = interchangePanel;
 		GridBagConstraints interchangePanelC = Util.constraint(GridBagConstraints.BOTH, 1.0, 4.0);
 		interchangePanelC.gridy = 1;
 		
@@ -87,11 +92,60 @@ public class TeamOverviewDialog extends TwoButtonDialog {
 		
 		BottomPanel.setBorder(BorderFactory.createEmptyBorder(k - 5,k,k - 5,k - 5));
 		
+		ListPanel.RightList.setModel(_developerTeamListModel);
+		ListPanel.RightList.invalidate();
+		
 		setSize(new Dimension(440, 280));
 	}
 	
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		
+		if (e.getStateChange() == ItemEvent.SELECTED)
+		{
+			int index = _teamInput.getSelectedIndex();
+			if (index != -1)
+			{
+				int newTeam = _teamComboBoxModel.getId(index);
+				_developerTeamListModel.setTeam(newTeam);
+					
+				ListPanel.RightList.invalidate();
+			}
+		}
+	}
+	
+	@Override
+	public void setVisible(boolean b) {
+	
+		if (b)
+		{
+			if (_teamComboBoxModel.getSize() > 0)
+			{
+				_teamInput.setEnabled(true);
+				_teamInput.setSelectedIndex(0);
+			}
+			else
+			{
+				_teamInput.setEnabled(false);
+			}
+		}
+		
+		super.setVisible(b);
+	}
+
+	/// team input box
+	private JComboBox _teamInput;
+	/// developer model
+	private DeveloperModel _developerModel;
+	/// current team developer list model
+	private DeveloperTeamListModel _developerTeamListModel;
+	/// team combo box model
+	private TeamComboBoxModel _teamComboBoxModel;
+	/// list interchange panel
+	private ListInterchangePanel ListPanel;
 	/// translation class field
 	private I18n i18n = Scrummer.getI18n(getClass());
 	/// serialization id
 	private static final long serialVersionUID = 1110268887633198339L;
+	
 }
