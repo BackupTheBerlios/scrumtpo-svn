bf5148848a09947bc83c9baee1043fd5d14285d2
katjaxc <katjaxc@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-07 10:27:36 +0000

    popravki na tabeli Sprint_PBI - sprememba primarnega ključa

--- Scrummer/trunk/baza.sql
+++ Scrummer/trunk/baza.sql
@@ -212,7 +212,7 @@ create table Sprint
 /*==============================================================*/
 create table Sprint_PBI 
 (
-   Measure_day	integer	AUTO_INCREMENT	not null,
+   Measure_day	integer	not null,
    PBI_id	integer	not null,
    Task_id	integer not null,
    Sprint_id	integer	not null,
@@ -221,7 +221,7 @@ create table Sprint_PBI
    Hours_remaining integer default '0' null,
    NbOpenImped	integer	default '0' null,
    NbClosedImped	integer	default '0'	null,
-   constraint PK_MEASURE primary key (Measure_day, PBI_id, Sprint_id, Task_id)
+   constraint PK_MEASURE primary key (Measure_day, PBI_id)
 ) CHARACTER SET utf8;
 
 /*==============================================================*/
