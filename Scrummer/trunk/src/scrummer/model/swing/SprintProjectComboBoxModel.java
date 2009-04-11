22aed088dbdfe08721cb81ba93040ad6cec19c74
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-11 20:32:54 +0000

    Dodal padajoči seznam za prikaz vseh iteracij na trenutnem projektu.

---
+++ Scrummer/trunk/src/scrummer/model/swing/SprintProjectComboBoxModel.java
@@ -0,0 +1,53 @@
+package scrummer.model.swing;
+
+import java.util.Vector;
+
+import scrummer.model.ProjectModel;
+import scrummer.model.SprintBacklogModelCommon;
+import scrummer.model.DBSchemaModel.IdValue;
+import scrummer.model.swing.base.IdValueComboBoxModel;
+
+/**
+ * Display all sprints on some project
+ */
+public class SprintProjectComboBoxModel extends IdValueComboBoxModel {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param sprintBacklogModelCommon common model for data ops.
+	 * @param projectModel project model for getting project id
+	 */
+	public SprintProjectComboBoxModel(SprintBacklogModelCommon sprintBacklogModelCommon, ProjectModel projectModel) {
+		_sprintBacklogModelCommon = sprintBacklogModelCommon;
+		_projectModel = projectModel;
+	}
+	
+	@Override
+	public void refresh() {
+		refreshRows();
+		super.refresh();
+	}
+
+	/**
+	 * Refresh rows
+	 */
+	private void refreshRows()
+	{
+		Vector<Integer> sprints = _sprintBacklogModelCommon.fetchSprints(_projectModel.getCurrentProjectId());
+		Vector<IdValue> duplicated = new Vector<IdValue>();
+		for (int i = 0; i < sprints.size(); i++)
+		{
+			IdValue current = new IdValue(sprints.get(i), sprints.get(i).toString());
+			duplicated.add(current);
+		}
+		setValues(duplicated);
+	}
+
+	/// project model
+	private ProjectModel _projectModel;
+	/// common sprint backlog operations model
+	private SprintBacklogModelCommon _sprintBacklogModelCommon;
+	/// serialization id
+	private static final long serialVersionUID = 8567946224297304431L;
+}
