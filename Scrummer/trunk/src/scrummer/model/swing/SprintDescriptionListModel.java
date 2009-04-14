abff82fb07185c24c9def89ad8497c4a96e9f1aa
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-14 19:14:24 +0000

    Dodal model za seznam iteracij na projektu.

---
+++ Scrummer/trunk/src/scrummer/model/swing/SprintDescriptionListModel.java
@@ -0,0 +1,45 @@
+package scrummer.model.swing;
+
+import scrummer.model.ProjectModel;
+import scrummer.model.SprintBacklogModelCommon;
+import scrummer.model.swing.base.IdValueListModel;
+
+/**
+ * A list model that displays Sprint descriptions while also holding corresponding id's on 
+ * current project. 
+ */
+public class SprintDescriptionListModel extends IdValueListModel {
+
+	/**
+	 * Constructor
+	 * 
+	 * @param sprintBacklogCommon common sprint data ops.
+	 * @param projectModel project model
+	 */
+	public SprintDescriptionListModel(SprintBacklogModelCommon sprintBacklogCommon, ProjectModel projectModel) {
+			
+		_sprintBacklogCommon = sprintBacklogCommon;
+		_projectModel = projectModel;
+	}
+	
+	@Override
+	public void refresh() {
+		refreshRows();
+		super.refresh();
+	}
+
+	/**
+	 * Fetch sprint id/description pairs from db.
+	 */
+	private void refreshRows()
+	{
+		setValues(_sprintBacklogCommon.fetchSprintDescriptions(_projectModel.getCurrentProjectId()));
+	}
+
+	/// sprint backlog model
+	private SprintBacklogModelCommon _sprintBacklogCommon;
+	/// project model
+	private ProjectModel _projectModel;
+	/// serialization id
+	private static final long serialVersionUID = 8434242422528715931L;
+}
