075b7d828daa65a6ce96f448fd01a234f7e7935e
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-21 20:30:43 +0000

    Dodela stran.

--- Scrummer/trunk/src/scrummer/ui/page/TaskPage.java
+++ Scrummer/trunk/src/scrummer/ui/page/TaskPage.java
@@ -2,24 +2,29 @@ package scrummer.ui.page;
 
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
-
 import scrummer.Scrummer;
 import scrummer.model.Models;
 import scrummer.model.SprintBacklogModel;
 import scrummer.model.swing.AllTaskTableModel;
-import scrummer.model.swing.SprintBacklogTableModel;
-import scrummer.model.swing.TaskTableModel;
 import scrummer.ui.MainFrame;
+import scrummer.ui.Util;
+import scrummer.ui.dialog.TaskAddDialog;
+import scrummer.uicomponents.AddEditRemovePanel;
 import scrummer.uicomponents.NiceTable;
 
 /**
  * A page that displays all tasks on current project and sprint
  */
-public class TaskPage extends BasePage {
+public class TaskPage 
+	extends BasePage 
+	implements ActionListener {
 
 	/**
 	 * Constructor
@@ -36,18 +41,50 @@ public class TaskPage extends BasePage {
 		_taskTableModel = _sprintBacklogModel.getTaskTableModel();
 		_taskTableModel.refresh();
 		
+		Box box = new Box(BoxLayout.Y_AXIS);
+		int k = 1;
+		box.setBorder(BorderFactory.createEmptyBorder(k, k, k, k));
+		
+		AddEditRemovePanel toolbar = new AddEditRemovePanel();
+		toolbar.addActionListener(this);
+		
 		NiceTable taskTable = new NiceTable();
 		taskTable.setModel(_taskTableModel);
 		taskTable.setBackground(Color.WHITE);
+		taskTable.setAdjacentComponents
+			(toolbar.Add, toolbar.Remove);
 		
 		JScrollPane scrollPane = new JScrollPane(taskTable);				
 		scrollPane.setBackground(Color.WHITE);
-		scrollPane.setBorder(BorderFactory.createLineBorder(Color.RED));
 		
 		setBackground(Color.WHITE);
-		add(scrollPane);
+		scrollPane.getViewport().setBackground(Color.WHITE);
+		
+		box.add(toolbar);
+		box.add(scrollPane);
+		
+		add(box);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String cmd = e.getActionCommand();
+		if (cmd == "Add")
+		{
+			TaskAddDialog dialog = new TaskAddDialog(getMainFrame());
+			Util.centre(dialog);
+			dialog.setVisible(true);
+		}
+		else if (cmd == "Edit")
+		{
+			
+		}
+		else if (cmd == "Remove")
+		{
+			
+		}
+	}
+	
 	/// sprint backlog model
 	private SprintBacklogModel _sprintBacklogModel;
 	/// task table model
