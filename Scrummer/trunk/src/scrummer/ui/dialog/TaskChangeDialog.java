637f17164648a86b3c7dcceb4ee6dc9d62aa4b06
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-04-25 11:25:02 +0000

    Usposobil osveževanje.

--- Scrummer/trunk/src/scrummer/ui/dialog/TaskChangeDialog.java
+++ Scrummer/trunk/src/scrummer/ui/dialog/TaskChangeDialog.java
@@ -1,24 +1,74 @@
 package scrummer.ui.dialog;
 
 import java.awt.Frame;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.xnap.commons.i18n.I18n;
 import scrummer.Scrummer;
+import scrummer.enumerator.DataOperation;
+import scrummer.enumerator.TaskOperation;
+import scrummer.listener.TaskListener;
+import scrummer.model.DBSchemaModel.TaskEnum;
+import scrummer.ui.Util;
 
 /**
  * Task modification dialog
  */
-public class TaskChangeDialog extends TaskDialog {
+public class TaskChangeDialog
+	extends TaskDialog 
+	implements TaskListener {
 
 	/**
 	 * Constructor
 	 * @param owner owning frame
+	 * @param taskId task id
 	 */
-	public TaskChangeDialog(Frame owner) {
+	public TaskChangeDialog(Frame owner, int taskId) {
 		super(owner);
 	
 		setTitle(i18n.tr("Change Task"));
+		_taskModel.addTaskListener(this);
 		
-				
+		_descriptionInput.setText(
+			_taskModel.getString(TaskEnum.TaskDescription, taskId));
+		_pbiInput.selectId(_taskModel.getInteger(TaskEnum.PBIId, taskId));
+		_parentInput.selectId(_taskModel.getInteger(TaskEnum.TaskParentId, taskId));
+		_taskEmployeeInput.selectId(_taskModel.getInteger(TaskEnum.EmployeeId, taskId));
+		_taskTeamInput.selectId(_taskModel.getInteger(TaskEnum.TeamId, taskId));
+		_taskTypeInput.selectId(_taskModel.getInteger(TaskEnum.TaskTypeId, taskId));
+		_taskStatusInput.selectId(_taskModel.getInteger(TaskEnum.TaskStatusId, taskId));
+		
+		SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+		Date d = _taskModel.getDate(TaskEnum.TaskDate, taskId);
+		_dateInput.setText((d == null) ? "" : df.format(d));
+		_taskActiveInput.setSelectedIndex((_taskModel.getBoolean(TaskEnum.TaskActive, taskId)) ? 0 : 1);		
+	}
+
+	@Override
+	public void operationFailed(DataOperation type, TaskOperation identifier, String message) {
+		if ((type == DataOperation.Update) && (identifier == TaskOperation.Task))
+		{
+			Util.showError(this, i18n.tr("Error while updating task: " + message), i18n.tr("Error"));
+		}
+	}
+	
+	@Override
+	public void operationSucceeded(DataOperation type, TaskOperation identifier, String message) {
+		if ((type == DataOperation.Update) && (identifier == TaskOperation.Task))
+		{
+			setVisible(false);
+		}
+	}
+	
+	@Override
+	public void setVisible(boolean b) {
+		
+		if (!b)
+		{
+			_taskModel.removeTaskListener(this);
+		}
+		
+		super.setVisible(b);
 	}
 
 	/// translation class field
