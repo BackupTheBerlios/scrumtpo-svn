312e8640fd04bd445499ebb820c3a694a60d3ca1
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-05-02 20:28:05 +0000

    Dodal id-ja za vrsto in stanje.

--- Scrummer/trunk/src/scrummer/enumerator/ImpedimentOperation.java
+++ Scrummer/trunk/src/scrummer/enumerator/ImpedimentOperation.java
@@ -6,6 +6,10 @@ package scrummer.enumerator;
 public enum ImpedimentOperation {
 	/// impediment related operation
 	Impediment,
+	/// impediment type related op
+	ImpedimentType,
+	/// impediment status related op
+	ImpedimentStatus,
 	/// team related operation
 	Team,
 	/// sprint related operations
