a29cf6eeb827df57f9af377e102c31aae920e931
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-05-05 12:30:42 +0000

    Finished dialog.

--- Scrummer/trunk/src/scrummer/ui/dialog/ImpedimentsChangeDialog.java
+++ Scrummer/trunk/src/scrummer/ui/dialog/ImpedimentsChangeDialog.java
@@ -1,12 +1,9 @@
 package scrummer.ui.dialog;
 
-import java.awt.Frame;
 import java.awt.event.ActionEvent;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import org.xnap.commons.i18n.I18n;
@@ -14,42 +11,52 @@ import scrummer.Scrummer;
 import scrummer.enumerator.DataOperation;
 import scrummer.enumerator.ImpedimentOperation;
 import scrummer.listener.ImpedimentListener;
-import scrummer.model.ImpedimentModel;
 import scrummer.model.DBSchemaModel.ImpedimentEnum;
-import scrummer.model.swing.ImpedimentComboBoxModel;
 import scrummer.ui.Util;
+import scrummer.ui.Validate;
 import scrummer.uicomponents.SelectedTextField;
-import scrummer.uicomponents.TwoButtonDialog;
 
-public class ImpedimentsChangeDialog extends TwoButtonDialog implements ImpedimentListener 
+/**
+ * Impediment change dialog
+ */
+public class ImpedimentsChangeDialog 
+	extends ImpedimentDialog 
+	implements ImpedimentListener 
 {
 	/**
 	 * Constructor
 	 * 
 	 * @param owner owner form
 	 */
-	public ImpedimentsChangeDialog(Frame owner, int impId)
+	public ImpedimentsChangeDialog(JFrame owner, int impId)
 	{
-		super(owner, ModalityType.APPLICATION_MODAL);
+		super(owner);
 		
 		_impId = impId;
 		setTitle(i18n.tr("Change impediment"));
+		_impedimentModel = Scrummer.getModels().getImpedimentModel();
 		_impedimentModel.addImpedimentListener(this);
 		
-		_teamInput.setText(_impedimentModel.getString(ImpedimentEnum.TeamId, impId));
-		_sprintInput.setText(_impedimentModel.getString(ImpedimentEnum.SprintId, impId));
-		_sprintInput.setText(_impedimentModel.getString(ImpedimentEnum.EmployeeId, impId));
-		_employeeInput.setText(_impedimentModel.getString(ImpedimentEnum.TaskId, impId));
-		_descInput.setText(_impedimentModel.getString(ImpedimentEnum.ImpedimentDescription, impId));
-		_typeInput.setText(_impedimentModel.getString(ImpedimentEnum.ImpedimentType, impId));
-		_statusInput.setText(_impedimentModel.getString(ImpedimentEnum.ImpedimentStatus, impId));
-		_ageInput.setText(_impedimentModel.getString(ImpedimentEnum.ImpedimentAge, impId));
+		int teamId = _impedimentModel.getInteger(ImpedimentEnum.TeamId, impId); 
+		_teamInput.selectId(teamId);
+		int empId = _impedimentModel.getInteger(ImpedimentEnum.EmployeeId, impId); 
+		_empInput.selectId(empId);
+		_descriptionTextField.setText(_impedimentModel.getString(ImpedimentEnum.ImpedimentDescription, impId));
+		int typeId = _impedimentModel.getInteger(ImpedimentEnum.ImpedimentType, impId); 
+		_impTypeInput.selectId(typeId);
+		int statusId = _impedimentModel.getInteger(ImpedimentEnum.ImpedimentStatus, impId);
+		_impStatusInput.selectId(statusId);
+		
+		_ageTextField.setText(new Integer(_impedimentModel.getInteger(ImpedimentEnum.ImpedimentAge, impId)).toString());
 			
 		SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
 		Date d1 = _impedimentModel.getDate(ImpedimentEnum.ImpedimentStart, impId);
-		_startInput.setText((d1 == null) ? "" : df.format(d1));
+		
+		_startTextField.setText((d1 == null) ? "" : df.format(d1));
 		Date d2 = _impedimentModel.getDate(ImpedimentEnum.ImpedimentEnd, impId);
-		_endInput.setText((d2 == null) ? "" : df.format(d2));
+		_endTextField.setText((d2 == null) ? "" : df.format(d2));
+		
+		setSize(570, 310);		
 	}
 	
 	/** 
@@ -73,164 +80,27 @@ public class ImpedimentsChangeDialog extends TwoButtonDialog implements Impedime
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
 		if (e.getActionCommand() == "StandardDialog.OK")
-		{
-			SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy"); 
-			java.util.Date startI, endI;
-			try {
-				String team = _teamInput.getText().trim();
-				String sprint = _sprintInput.getText().trim();
-				String employee = _employeeInput.getText().trim();
-				String task = _taskInput.getText().trim();
-				String desc = _descInput.getText().trim();
-				String type = _typeInput.getText().trim();
-				String status = _statusInput.getText().trim();
-				String age = _ageInput.getText().trim();
-				
-				if (team.length() > 0)
-				{
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentTeam(id, team);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's team."), i18n.tr("Error"));
-					}
-				}
-				if(sprint.length() > 0)
-				{
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentSprint(id, sprint);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's sprint."), i18n.tr("Error"));
-					}
-				}
-				if(employee.length() > 0)
-				{
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentEmployee(id, employee);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's employee."), i18n.tr("Error"));
-					}
-				}
-				if(task.length() > 0)
-				{
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentTask(id, task);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's task."), i18n.tr("Error"));
-					}
-				}
-				if(desc.length() > 0)
-				{
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentDesc(id, desc);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's description."), i18n.tr("Error"));
-					}
-				}
-				if(type.length() > 0)
-				{
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentType(id, type);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's type."), i18n.tr("Error"));
-					}
-				}
-				if(status.length() > 0)
-				{
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentStatus(id, status);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's status."), i18n.tr("Error"));
-					}
-				}
-				if(_startInput.getText().trim().length() > 0)
-				{
-					startI = dateFormat.parse(_startInput.getText());
-					java.sql.Date start = new java.sql.Date(startI.getTime());
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentStart(id, start);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's start."), i18n.tr("Error"));
-					}
-				}
-				if(_endInput.getText().trim().length() > 0)
-				{
-					endI = dateFormat.parse(_endInput.getText());
-					java.sql.Date end = new java.sql.Date(endI.getTime());
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentEnd(id, end);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's end."), i18n.tr("Error"));
-					}
-				}
-				if(age.length() > 0)
-				{
-					int selected = _impInput.getSelectedIndex();
-					if (selected != -1)
-					{
-						int id = _impedimentComboModel.getId(selected);
-						_impedimentModel.setImpedimentAge(id, age);
-					}
-					else
-					{
-						Util.showError(this, i18n.tr("Some impediment must be selected to change it's age."), i18n.tr("Error"));
-					}
-				}
-				/*else
-				{
-					Util.showError(this, i18n.tr("Impediment must be at least one character long."), i18n.tr("Error"));
-				}*/
-				
-			} catch (ParseException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
+		{			
+			int team = _teamInput.getSelectedId();
+			int employee = _empInput.getSelectedId();
+			int task = _taskInput.getSelectedId();
+			if (!Validate.emptyMsg(_descriptionTextField, this)) return;
+			String desc = _descriptionTextField.getText().trim();
+			
+			int type = _impTypeInput.getSelectedId();
+			int status = _impStatusInput.getSelectedId();
+			int age = Validate.number(_ageTextField);
+			
+			Date startDate = Validate.date(_startTextField, i18n.tr("Wrong starting date"), this);
+			Date endDate = Validate.date(_endTextField, i18n.tr("Wrong ending date."), this);
+			
+			if ((startDate == null) || (endDate == null)) return; 
+			
+			int id = _impId;
+			_impedimentModel.update(id, team, employee, task, desc, type, status, startDate, endDate, age);
+			
+			setVisible(false);
 		}
 		else
 		{
@@ -240,73 +110,25 @@ public class ImpedimentsChangeDialog extends TwoButtonDialog implements Impedime
 
 	@Override
 	public void operationSucceeded(DataOperation type, ImpedimentOperation identifier, String message) {
-		switch (type)
+		if ((type == DataOperation.Update) && (identifier == ImpedimentOperation.Impediment))
 		{
-		case Update:
-		
-			switch (identifier)
-			{
-			case Team:
-				_impedimentComboModel.refresh();
-				_impInput.setSelectedIndex(0);
-				setVisible(false);
-				break;
-			}
-			break;
+			_impedimentComboModel.refresh();			
+			setVisible(false);
 		}
 	}
 	
 	@Override
 	public void operationFailed(DataOperation type, ImpedimentOperation identifier, String message) {
-		
-		switch (type)
+		if ((type == DataOperation.Update) && (identifier == ImpedimentOperation.Impediment))
 		{
-		case Update:
-			switch (identifier)
-			{
-			case Team:
-				Util.showError(this, 
-					i18n.tr("An error has occurred when setting team name") + ": " + message, 
-					i18n.tr("Error"));
-				break;
-			}
-			break;
+			Util.showError(this, 
+				i18n.tr("An error has occurred when setting updating impediment") + ": " + message, 
+				i18n.tr("Error"));
 		}
-	}
-
-	@Override
-	public void setVisible(boolean b) {
-		
-		if (!b)
-		{
-			_impedimentModel.removeImpedimentListener(this);
-		}
-		else
-		{
-			if (_impedimentComboModel.getSize() == 0)
-			{
-				_teamInput.setEnabled(false);
-			}
-			else
-			{
-				_impInput.setEnabled(true);
-				_impInput.setSelectedIndex(0);
-			}
-		}
-		
-		super.setVisible(b);
 	}
 	
 	/// impediment id
-	private int _impId;
-	/// impediment model
-	private ImpedimentModel _impedimentModel;
-	/// all impediments in combo box
-	private ImpedimentComboBoxModel _impedimentComboModel;
-	/// team new name input
-	private JTextField _teamInput, _sprintInput, _taskInput, _employeeInput, _descInput, _typeInput, _statusInput, _startInput, _endInput, _ageInput;
-	/// team input combo box
-	private JComboBox _impInput;
+	private int _impId;	
 	/// translation class field
 	private I18n i18n = Scrummer.getI18n(getClass());
 	/// serialization id
