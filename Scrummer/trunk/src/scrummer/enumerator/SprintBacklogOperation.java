b6d49edc335c10148cd8cf3b948938e850b64106
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-05-12 08:58:11 +0000

    Dodal operacijo za vse v zvezi s sprintpbi tabelo.

--- Scrummer/trunk/src/scrummer/enumerator/SprintBacklogOperation.java
+++ Scrummer/trunk/src/scrummer/enumerator/SprintBacklogOperation.java
@@ -7,6 +7,8 @@ public enum SprintBacklogOperation {
 	/// sprint backlog related operation
 	SprintBacklog,
 	Sprint,
+	/// sprint pbi related table data change
+	SprintPBI,
 	PBI,
 	Task,
 	Employee,
