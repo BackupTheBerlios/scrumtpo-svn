81695fecec70855b8c842e1e34c0574d237231b8
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-05-14 12:23:20 +0000

    Popravil precej težko napako.

--- Scrummer/trunk/src/scrummer/util/Operations.java
+++ Scrummer/trunk/src/scrummer/util/Operations.java
@@ -22,11 +22,11 @@ public class Operations {
 	public static class ProjectOperation extends Operation<scrummer.enumerator.ProjectOperation, ProjectListener> {
 		@Override
 		protected void opFailure(ProjectListener listener, DataOperation type, scrummer.enumerator.ProjectOperation identifier, String message) {			
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(ProjectListener listener, DataOperation type, scrummer.enumerator.ProjectOperation identifier, String message) {
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
@@ -37,11 +37,11 @@ public class Operations {
 	public static class DeveloperOperation extends Operation<scrummer.enumerator.DeveloperOperation, DeveloperListener> {
 		@Override
 		protected void opFailure(DeveloperListener listener, DataOperation type, scrummer.enumerator.DeveloperOperation identifier, String message) {
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(DeveloperListener listener, DataOperation type, scrummer.enumerator.DeveloperOperation identifier, String message) {
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
@@ -52,11 +52,11 @@ public class Operations {
 	public static class ImpedimentOperation extends Operation<scrummer.enumerator.ImpedimentOperation, ImpedimentListener> {
 		@Override
 		protected void opFailure(ImpedimentListener listener, DataOperation type, scrummer.enumerator.ImpedimentOperation identifier, String message) {
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(ImpedimentListener listener, DataOperation type, scrummer.enumerator.ImpedimentOperation identifier, String message) {
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
@@ -67,11 +67,11 @@ public class Operations {
 	public static class AbsenceTypeOperation extends Operation<scrummer.enumerator.AbsenceTypeOperation, AbsenceTypeListener> {
 		@Override
 		protected void opFailure(AbsenceTypeListener listener, DataOperation type, scrummer.enumerator.AbsenceTypeOperation identifier, String message) {			
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(AbsenceTypeListener listener, DataOperation type, scrummer.enumerator.AbsenceTypeOperation identifier, String message) {
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
@@ -82,11 +82,11 @@ public class Operations {
 	public static class SprintBacklogOperation extends Operation<scrummer.enumerator.SprintBacklogOperation, SprintBacklogListener> {
 		@Override
 		protected void opFailure(SprintBacklogListener listener,  DataOperation type, scrummer.enumerator.SprintBacklogOperation identifier, String message) {			
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(SprintBacklogListener listener, DataOperation type, scrummer.enumerator.SprintBacklogOperation identifier, String message) {
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
@@ -97,11 +97,11 @@ public class Operations {
 	public static class ProductBacklogOperation extends Operation<scrummer.enumerator.ProductBacklogOperation, ProductBacklogListener> {
 		@Override
 		protected void opFailure(ProductBacklogListener listener, DataOperation type, scrummer.enumerator.ProductBacklogOperation identifier, String message) {
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(ProductBacklogListener listener, DataOperation type, scrummer.enumerator.ProductBacklogOperation identifier, String message) {			
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
@@ -112,45 +112,45 @@ public class Operations {
 	public static class AdminDaysOperation extends Operation<scrummer.enumerator.AdminDaysOperation, AdminDaysListener> {
 		@Override
 		protected void opFailure(AdminDaysListener listener, DataOperation type, scrummer.enumerator.AdminDaysOperation identifier, String message) {			
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 
 		@Override
 		protected void opSuccess(AdminDaysListener listener, DataOperation type, scrummer.enumerator.AdminDaysOperation identifier, String message) {
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
 	public static class TaskStatusOperation extends Operation<scrummer.enumerator.TaskStatusOperation, TaskStatusListener> {
 		@Override
 		protected void opFailure(TaskStatusListener listener, DataOperation type, scrummer.enumerator.TaskStatusOperation identifier, String message) {
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(TaskStatusListener listener, DataOperation type, scrummer.enumerator.TaskStatusOperation identifier, String message) {
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
 	public static class TaskTypeOperation extends Operation<scrummer.enumerator.TaskTypeOperation, TaskTypeListener> {
 		@Override
 		protected void opFailure(TaskTypeListener listener, DataOperation type, scrummer.enumerator.TaskTypeOperation identifier, String message) {
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(TaskTypeListener listener, DataOperation type, scrummer.enumerator.TaskTypeOperation identifier, String message) {
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
 	public static class TaskOperation extends Operation<scrummer.enumerator.TaskOperation, TaskListener> {
 		@Override
 		protected void opFailure(TaskListener listener, DataOperation type, scrummer.enumerator.TaskOperation identifier, String message) {			
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(TaskListener listener, DataOperation type, scrummer.enumerator.TaskOperation identifier, String message) {			
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 	
@@ -161,11 +161,11 @@ public class Operations {
 	public static class MetricOperation extends Operation<scrummer.enumerator.MetricOperation, MetricListener> {
 		@Override
 		protected void opFailure(MetricListener listener, DataOperation type, scrummer.enumerator.MetricOperation identifier, String message) {
-			listener.operationSucceeded(type, identifier, message);
+			listener.operationFailed(type, identifier, message);
 		}
 		@Override
 		protected void opSuccess(MetricListener listener, DataOperation type, scrummer.enumerator.MetricOperation identifier, String message) {			
-			listener.operationFailed(type, identifier, message);
+			listener.operationSucceeded(type, identifier, message);
 		}
 	}
 }
