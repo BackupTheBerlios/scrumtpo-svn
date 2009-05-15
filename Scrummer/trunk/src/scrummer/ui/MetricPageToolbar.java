715b436ba0374c8f022cfd9cb7894b761b70dcbd
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-05-15 15:41:27 +0000

    Deloma usposobil novo orodjarno za metrike.

--- Scrummer/trunk/src/scrummer/ui/MetricPageToolbar.java
+++ Scrummer/trunk/src/scrummer/ui/MetricPageToolbar.java
@@ -2,67 +2,109 @@ package scrummer.ui;
 
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.JPanel;
-import org.xnap.commons.i18n.I18n;
+
 import scrummer.Scrummer;
-import scrummer.model.DeveloperModel;
+import scrummer.model.MetricModel;
 import scrummer.model.Models;
-import scrummer.model.ProductBacklogModel;
-import scrummer.model.TaskModel;
-import scrummer.model.swing.SprintPBITableModel;
+import scrummer.model.swing.MetricTableModel;
+import scrummer.uicomponents.AddEditRemovePanel;
 import scrummer.uicomponents.StandardComboBox;
 
 /**
- * This page displays employee and pbi combo boxes, that affect underlying 
+ * Toolbar that displays either Sprint or Task input measurements.
+ * It also handles addition/edit/removal of measurements.
  */
 public class MetricPageToolbar 
-	extends JPanel 
-	implements ActionListener {
-
+	extends JPanel {
+	
 	public MetricPageToolbar() {
-		setBackground(Color.WHITE);
-		setLayout(new FlowLayout(FlowLayout.LEFT));
-		
+		setLayout(new GridLayout(1,2));
+	
 		Models m = Scrummer.getModels();
-		TaskModel taskModel = m.getTaskModel();
-		DeveloperModel developerModel = m.getDeveloperModel(); 
-		ProductBacklogModel productModel = m.getProductBacklogModel();
-		_sprintPbiTableModel = m.getSprintBacklogModel().getSprintPBITableModel();
+		_metricModel = m.getMetricModel();
+		_metricTableModel = _metricModel.getMetricTableModel();
 		
-		FormBuilder fb = new FormBuilder(this, FormBuilder.Layout.LeftToRight);
-		
-		_taskInput =
-			fb.addComboBoxInput(i18n.tr("Task") + ":");
-		_taskInput.addActionListener(this);
-		_taskInput.setIVModel(taskModel.getProjectSprintTaskComboBoxModel());
-		
-		_employeeInput = 
-			fb.addComboBoxInput(i18n.tr("Employee") + ":");
-		_employeeInput.addActionListener(this);
-		_employeeInput.setIVModel(developerModel.getEmployeeComboBoxModel());
-		
+		JPanel left = new JPanel();
+		left.setBackground(Color.WHITE);
+		FormBuilder fb = new FormBuilder(left, FormBuilder.Layout.LeftToRight);
 		fb.setCellSpacing(5, 5);
+		_metricInput =
+			fb.addComboBoxInput(i18n.tr("Metric") + ":");		
+		_metricSelectionInput = 
+			fb.addComboBoxInput(i18n.tr("Metric Type") + ":");
+		_specificMetricInput =
+			fb.addComboBoxInput("");
+		add(left);
+		
+		AddEditRemovePanel right = new AddEditRemovePanel();		
+		add(right);
+		
+		_metricSelectionInput.addItem("Sprint");
+		_metricSelectionInput.addItem("Task");
+		
+		_metricInput.setIVModel(_metricModel.getMetricComboBoxModel());		
+		
+		_metricSelectionInput.setSelectedIndex(0);
+	}	
+	
+	/**
+	 * Add item listener to all combo boxes
+	 * @param listener listener
+	 */
+	public void addItemListener(ItemListener listener) {
+		_specificMetricInput.addItemListener(listener);
+		_metricSelectionInput.addItemListener(listener);
+		_metricInput.addItemListener(listener);
 	}
 	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == _employeeInput) {
-			_sprintPbiTableModel.setEmployeeId(_employeeInput.getSelectedId());
-		} else {
-			_sprintPbiTableModel.setTaskId(_taskInput.getSelectedId());
-		}
+	/**
+	 * Remove listener from all combo boxes
+	 * @param listener
+	 */
+	public void removeItemListener(ItemListener listener) {
+		_specificMetricInput.removeItemListener(listener);
+		_metricSelectionInput.removeItemListener(listener);
+		_metricInput.removeItemListener(listener);
 	}
 	
-	/// link between sprint and pbi table model
-	SprintPBITableModel _sprintPbiTableModel;
-	/// employee and pbi input
-	StandardComboBox _employeeInput, _taskInput;
+	/**
+	 * Add action listener to add/edit/remove button events
+	 * @param listener listener
+	 */
+	public void addActionListener(ActionListener listener) {
+		_addEditRemovePanel.addActionListener(listener);
+	}
+	
+	/**
+	 * Remove action listener from add/edit/remove button events
+	 * @param listener listener
+	 */
+	public void removeActionListener(ActionListener listener) {
+		_addEditRemovePanel.removeActionListener(listener);
+	}
+	
+	/// metric model
+	private MetricModel _metricModel;
+	/// metric table model
+	private MetricTableModel _metricTableModel;
 	/// translation class field
-	private I18n i18n = Scrummer.getI18n(getClass());
-	/// serializatino id
-	private static final long serialVersionUID = 5809025304631771019L;
-	
+	private org.xnap.commons.i18n.I18n i18n = Scrummer.getI18n(getClass());
+	/// this combo box controls what kind of information will be available next
+	private StandardComboBox _metricSelectionInput;
+	/// user can select either a sprint or a task, etc. from this list
+	private StandardComboBox _specificMetricInput;
+	/// metric type cmbo box
+	private StandardComboBox _metricInput;
+	/// add/edit/remove panel
+	private AddEditRemovePanel _addEditRemovePanel;
+	/// serialization id
+	private static final long serialVersionUID = -8207088791556687634L;
+
 }
