8babb2cf29fdfa4ea58ac8fc2f10a2bb2507647c
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-05-18 14:03:22 +0000

    Dodelal model.

--- Scrummer/trunk/src/scrummer/model/swing/MetricTableModel.java
+++ Scrummer/trunk/src/scrummer/model/swing/MetricTableModel.java
@@ -1,5 +1,10 @@
 package scrummer.model.swing;
 
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Vector;
 
 import javax.swing.table.DefaultTableModel;
@@ -49,21 +54,54 @@ public class MetricTableModel extends DefaultTableModel {
 	public void refresh() {		
 		refreshTableData();
         fireTableDataChanged();
-        fireTableDataChanged();
+	}
+	
+	/**
+	 * @param row row index
+	 * @return id in database
+	 */
+	public int getId(int row) {
+		System.out.println(_rows.get(row).get(0).toString());
+		String s = _rows.get(row).get(0).toString();
+		if (s.trim().length() == 0) { s = "0"; }
+		return Integer.parseInt(s.trim());
+	}
+	
+	/**
+	 * Fetch date for some row
+	 * @param row row
+	 * @return converted date
+	 */
+	public Date getDate(int row) {
+		DateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+		try {
+			return df.parse(_rows.get(row).get(1).toString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Fetch measure value at specified row
+	 * @param row row from which to fetch it
+	 * @return measure
+	 */
+	public BigDecimal getValue(int row) {
+		return new BigDecimal(_rows.get(row).get(2).toString());
 	}
 	
 	/**
 	 * Refresh table data
 	 */
-	private void refreshTableData()
-	{
-		if (_id != -1) {
+	private void refreshTableData() {
+		if ((_metricId != -1) && (_objectId != -1)) {
 			switch (_metricType) {
 			case Sprint:
-				_rows = _metricModelCommon.fetchSprintMetric(_id);
+				_rows = _metricModelCommon.fetchSprintMetric(_objectId, _metricId);				
 				break;
 			case Task:
-				_rows = _metricModelCommon.fetchTaskMetric(_id);
+				_rows = _metricModelCommon.fetchTaskMetric(_objectId, _metricId);
 				break;
 			}
 	        _rowCount = _rows.size();
@@ -93,7 +131,7 @@ public class MetricTableModel extends DefaultTableModel {
 
 	@Override
 	public Object getValueAt(int row, int column) {
-		return _rows.get(row).get(column);
+		return _rows.get(row).get(column+1);
 	}
 
 	@Override
@@ -108,12 +146,14 @@ public class MetricTableModel extends DefaultTableModel {
 	
 	/**
 	 * Set different metric type
-	 * @param metric metric type
-	 * @param id specific
+	 * @param metricType metric type
+	 * @param metricId metric id
+	 * @param id specific object id
 	 */
-	public void setMetricType(MetricType metric, int id) {
-		_metricType = metric;
-		_id = id;
+	public void setMetricType(MetricType metricType, int metricId, int objectId) {
+		_metricType = metricType;
+		_metricId = metricId;
+		_objectId = objectId;
 		refresh();
 	}
 	
@@ -122,7 +162,9 @@ public class MetricTableModel extends DefaultTableModel {
 	/// displayed metric type
 	private MetricType _metricType = MetricType.Sprint;
 	/// id of sprint, task, pbi or release
-	private int _id = -1;
+	private int _metricId = -1;
+	/// object id designates either task, sprint etc.
+	private int _objectId = -1;
 	/// column count
 	private int _columnCount = 0;
 	/// row count
