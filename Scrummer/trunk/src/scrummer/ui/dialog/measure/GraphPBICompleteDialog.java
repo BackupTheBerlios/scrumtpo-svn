09f6dccbe24a9d94d867f4978ec45743d00a1bc1
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-05-31 18:43:22 +0000

    Popravil obrazec.

--- Scrummer/trunk/src/scrummer/ui/dialog/measure/GraphPBICompleteDialog.java
+++ Scrummer/trunk/src/scrummer/ui/dialog/measure/GraphPBICompleteDialog.java
@@ -2,6 +2,7 @@ package scrummer.ui.dialog.measure;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 
 import org.xnap.commons.i18n.I18n;
@@ -12,14 +13,19 @@ import scrummer.enumerator.MetricOperation;
 import scrummer.listener.MetricListener;
 import scrummer.model.MetricModel;
 import scrummer.model.Models;
+import scrummer.model.ReleaseModel;
+import scrummer.model.SprintBacklogModel;
+import scrummer.ui.FormBuilder;
 import scrummer.ui.SprintDialog;
 import scrummer.ui.Util;
+import scrummer.uicomponents.StandardComboBox;
+import scrummer.uicomponents.TwoButtonDialog;
 
 /**
- * Dialog for selection of sprint for pbi sprint completion indicator
+ * Dialog for selection of sprint and release for pbi sprint completion indicator
  */
 public class GraphPBICompleteDialog 
-	extends SprintDialog 
+	extends TwoButtonDialog 
 	implements MetricListener {
 
 	public GraphPBICompleteDialog(JFrame owner) {
@@ -28,13 +34,36 @@ public class GraphPBICompleteDialog
 		_metricModel = m.getMetricModel();
 		_metricModel.addMetricListener(this);
 		setTitle(i18n.tr("PBI Completion"));
+		
+		int k = 10;
+		Panel.setBorder(
+			Util.createSpacedTitleBorder(
+			k, k, k, k, 
+			i18n.tr("Release/Sprint Information"), 
+			2, k, k, k));
+		
+		FormBuilder fb = new FormBuilder(Panel);		
+		_sprintInput = 
+			fb.addComboBoxInput(i18n.tr("Sprint") + ":");
+		_releaseInput = 
+			fb.addComboBoxInput(i18n.tr("Release") + ":");
+				
+		SprintBacklogModel sbmodel = m.getSprintBacklogModel();
+		_sprintInput.setIVModel(sbmodel.getSprintProjectComboBoxModel());
+		
+		ReleaseModel releaseModel = m.getReleaseModel();
+		_releaseInput.setIVModel(releaseModel.getReleaseComboBoxModel());
+		
+		BottomPanel.setBorder(BorderFactory.createEmptyBorder(0, k, k, k - 3));
+		
+		setSize(320, 180);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String cmd = e.getActionCommand();
 		if (cmd.equals("StandardDialog.OK")) {
-			// _metricModel.calculateMonthlySPI(_sprintInput.getSelectedId());
+			_metricModel.calculatePBIComplete(_sprintInput.getSelectedId(), _releaseInput.getSelectedId());
 		} else {
 			super.actionPerformed(e);
 		}
@@ -65,6 +94,8 @@ public class GraphPBICompleteDialog
 		super.setVisible(b);
 	}
 
+	/// sprint input combo box
+	protected StandardComboBox _sprintInput, _releaseInput;
 	/// metric model
 	private MetricModel _metricModel;
 	/// translation class field
