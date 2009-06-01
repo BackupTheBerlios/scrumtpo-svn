c62ec371122776ceca24af0da97dbf2c04b56b97
crna_kava <crna_kava@4cee2a38-3165-0410-8684-98beef0be2ec>
2009-06-01 09:09:44 +0000

    Obrazci za druge metrike.

---
+++ Scrummer/trunk/src/scrummer/ui/dialog/measure/GraphAllDeveloperPollDialog.java
@@ -0,0 +1,95 @@
+package scrummer.ui.dialog.measure;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+
+import org.xnap.commons.i18n.I18n;
+
+import com.mysql.jdbc.Util;
+
+import scrummer.Scrummer;
+import scrummer.enumerator.DataOperation;
+import scrummer.enumerator.MetricOperation;
+import scrummer.listener.MetricListener;
+import scrummer.model.MetricModel;
+import scrummer.model.Models;
+import scrummer.ui.FormBuilder;
+import scrummer.uicomponents.StandardComboBox;
+import scrummer.uicomponents.TwoButtonDialog;
+
+/**
+ * Graph for asking for developer poll for all sprints
+ */
+public class GraphAllDeveloperPollDialog 
+	extends TwoButtonDialog 
+	implements MetricListener {
+
+	public GraphAllDeveloperPollDialog(JFrame owner) {
+		super(owner);
+		
+		int k = 10;
+		Panel.setBorder(
+			scrummer.ui.Util.createSpacedTitleBorder(
+			k, k, k, k, 
+			i18n.tr("Question Information"), 
+			2, k, k, k));
+		
+		Models m = Scrummer.getModels();
+		_metricModel = m.getMetricModel();
+		_metricModel.addMetricListener(this);
+		
+		FormBuilder	fb = new FormBuilder(Panel);		
+		_questionInput = fb.addComboBoxInput(i18n.tr("Question"));
+		
+		_questionInput.setIVModel(_metricModel.getQuestionDeveloperComboBoxModel());
+		
+		BottomPanel.setBorder(BorderFactory.createEmptyBorder(0, k, k, k - 3));
+		
+		setSize(320, 150);
+		setTitle(i18n.tr("Plot All Sprint Developer Poll"));
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String cmd = e.getActionCommand();
+		if (cmd.equals("StandardDialog.OK")) {
+			_metricModel.calculateAllSprintDeveloperPoll(_questionInput.getSelectedId());
+		} else {
+			super.actionPerformed(e);
+		}
+	}
+	
+	@Override
+	public void operationSucceeded(DataOperation type, MetricOperation identifier, String message) {
+		if ((type == DataOperation.Custom) && (identifier == MetricOperation.Graph)) {
+			setVisible(false);
+		}
+	}
+	
+	@Override
+	public void operationFailed(DataOperation type, MetricOperation identifier, String message) {
+		if ((type == DataOperation.Custom) && (identifier == MetricOperation.Graph)) {
+			scrummer.ui.Util.showError(this, i18n.tr("Error while calculating all developer marks poll: " + message), i18n.tr("Error"));
+		}
+	}
+	
+	@Override
+	public void setVisible(boolean b) {	
+		if (!b) {
+			_metricModel.removeMetricListener(this);
+		}
+		super.setVisible(b);
+	}
+
+	/// translation class field
+	private I18n i18n = Scrummer.getI18n(getClass());
+	/// metric model
+	private MetricModel _metricModel;
+	/// question input
+	private StandardComboBox _questionInput;
+	/// serialization id
+	private static final long serialVersionUID = -2927404943806468389L;
+
+}
